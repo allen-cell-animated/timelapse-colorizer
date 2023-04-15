fb7b020388a89bf9c5aa8f92abbdcfa6886895de
Cameron Fraser <cameron.fraser@alleninstitute.org>
2023-04-14 23:13:24 -0700

    add gradient creation function to `ColorRamp`

--- src/colorizer/ColorRamp.ts
+++ src/colorizer/ColorRamp.ts
@@ -14,7 +14,27 @@ export default class ColorRamp {
     this.texture.needsUpdate = true;
   }
 
-  // TODO add a method to generate a canvas with a horizontal gradient, for dropdowns
+  /** Creates a canvas filled in with this color ramp, to present as an option in a menu e.g. */
+  public createGradientCanvas(width: number, height: number, vertical = false): HTMLCanvasElement {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d")!;
+
+    if (this.colorStops.length < 2) {
+      ctx.fillStyle = `#${this.colorStops[0].getHexString()}`;
+    } else {
+      const gradient = ctx.createLinearGradient(0, 0, vertical ? 0 : width, vertical ? height : 0);
+      const step = 1 / (this.colorStops.length - 1);
+      this.colorStops.forEach((color, idx) => {
+        gradient.addColorStop(step * idx, `#${color.getHexString()}`);
+      });
+      ctx.fillStyle = gradient;
+    }
+
+    ctx.fillRect(0, 0, width, height);
+    return canvas;
+  }
 
   public dispose(): void {
     this.texture.dispose();
