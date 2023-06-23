e2cc00a4575ad9824baddfdf218f8d8f647388d8
Peyton Lee <peyton.lee@alleninstitute.org>
2023-06-23 10:05:27 -0700

    refactor: Changed let to const

--- src/colorizer/TimeControls.ts
+++ src/colorizer/TimeControls.ts
@@ -55,7 +55,7 @@ export default class TimeControls {
         // stop render loop if time controls have been disabled.
         return;
       }
-      let nextFrame = this.wrapFrame(this.canvas.getCurrentFrame() + 1);
+      const nextFrame = this.wrapFrame(this.canvas.getCurrentFrame() + 1);
 
       // do the necessary update
       await this.canvas.setFrame(nextFrame);
