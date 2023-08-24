0fa011f24dbc574d31e1b57bf1bd8628b32b75bf
Peyton Lee <peyton.lee@alleninstitute.org>
2023-08-24 13:50:06 -0700

    doc: Updated useDebounce documentation

--- src/colorizer/utils/react_utils.ts
+++ src/colorizer/utils/react_utils.ts
@@ -4,11 +4,27 @@ import { useEffect, useState } from "react";
  * Delays changes to a value until no changes have occurred for the
  * set delay period, in milliseconds. This is useful for delaying changes to state
  * when a value may update very quickly.
+ *
+ * Adapted from https://usehooks-ts.com/react-hook/use-debounce.
+ *
  * @param value The value to return.
  * @param delayMs The delay, in milliseconds.
  * @returns The `value` once no changes have occurred for `delay` milliseconds.
+ * @example
+ * ```
+ * const [value, setValue] = useState(0);
+ * const debouncedValue = useDebounce(value, 500);
  *
- * Adapted from https://usehooks-ts.com/react-hook/use-debounce.
+ * useEffect(() => {
+ *  // Some expensive operation
+ * }, [debouncedValue])
+ *
+ * return(
+ *  <div>
+ *      <p>{debouncedValue}</p>
+ *  </div>
+ * )
+ * ```
  */
 export function useDebounce<T>(value: T, delayMs?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
