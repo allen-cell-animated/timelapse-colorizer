ac507f6ad25be0beff6515f4f96a0fcbb160b5a7
Peyton Lee <peyton.lee@alleninstitute.org>
2023-09-06 13:55:57 -0700

    feat: Project configuration for running React tests

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import glsl from 'vite-plugin-glsl';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl(), react()]
 });
