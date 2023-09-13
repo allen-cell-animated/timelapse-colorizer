92ab3ff277cc3e2f1d84c8a10e30fc98574538b5
Peyton Lee <peyton.lee@alleninstitute.org>
2023-09-13 09:18:11 -0700

    feat: Added playback IconButtons, support for SVGs

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -5,12 +5,22 @@ import { COLOR_THEME_LIGHT } from "../constants/theme";
 
 type IconButtonProps = {
   onClick?: () => void;
+  disabled?: boolean;
 };
 
+/**
+ * Custom styled button intended to hold a single icon passed in as a child.
+ * @params onClick: The callback fired when a click event occurs.
+ *
+ * @example
+ * <IconButton onClick={myClickHandler}>
+ *    <PauseOutlined /> // From Antd Icons
+ * </IconButton>
+ */
 export default function IconButton(props: PropsWithChildren<IconButtonProps>): ReactElement {
   return (
     <ConfigProvider theme={{ components: { Button: { colorPrimaryActive: COLOR_THEME_LIGHT } } }}>
-      <Button type="primary" className={styles.iconButton}>
+      <Button type="primary" className={styles.iconButton} disabled={props.disabled} onClick={props.onClick}>
         {props.children}
       </Button>
     </ConfigProvider>
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,3 +1,4 @@
 export const COLOR_THEME = "var(--color-theme)";
 export const COLOR_THEME_DARK = "var(--color-theme-dark)";
 export const COLOR_THEME_LIGHT = "var(--color-theme-light)";
+export const COLOR_TEXT_DISABLED = "var(--color-text-disabled)";
