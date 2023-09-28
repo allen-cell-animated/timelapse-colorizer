f9090131f7ac0b4a90aa8bceef3c62333cc8d189
Peyton Lee <peyton.lee@alleninstitute.org>
2023-09-28 11:28:41 -0700

    feat: Grayscale presets, experimental collapse menus

--- src/components/DrawModeSelector.tsx
+++ src/components/DrawModeSelector.tsx
@@ -1,24 +1,46 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useRef } from "react";
 import styled from "styled-components";
 import LabeledDropdown from "./LabeledDropdown";
 import { DrawMode } from "../colorizer/ColorizeCanvas";
 import { Color as ThreeColor, ColorRepresentation } from "three";
-import { ColorPicker } from "antd";
+import { Collapse, CollapseProps, ColorPicker } from "antd";
 import { Color as AntdColor } from "@rc-component/color-picker";
+import { ColorRampData } from "../constants";
 
 type DrawModeSelectorProps = {
   label: string;
   selected: DrawMode;
   onChange: (mode: DrawMode, color: ThreeColor) => void;
   color: ThreeColor;
+  colorMap?: ColorRampData | null;
 };
 
-const defaultProps: Partial<DrawModeSelectorProps> = {};
+const defaultProps: Partial<DrawModeSelectorProps> = {
+  colorMap: null,
+};
 
 const MainLayout = styled.div`
   display: flex;
   flex-direction: column;
   gap: 6px;
+`;
+
+const MiniCollapse = styled(Collapse)`
+  line-height: 1.666667;
+  font-size: 12px;
+
+  // Style the collapse menu so it looks like Color Picker's default
+  & .ant-collapse-item .ant-collapse-header {
+    margin: 0;
+    padding: 0;
+  }
+
+  & .ant-collapse-item .ant-collapse-header .ant-collapse-expand-icon {
+    height: 20px;
+    color: rgba(0, 0, 0, 0.25);
+    padding-inline-end: 4px;
+    margin-inline-start: 4px;
+  }
 `;
 
 const HorizontalDiv = styled(MainLayout)`
@@ -28,6 +50,8 @@ const HorizontalDiv = styled(MainLayout)`
 
 export default function DrawModeSelector(propsInput: DrawModeSelectorProps): ReactElement {
   const props = { ...defaultProps, ...propsInput } as Required<DrawModeSelectorProps>;
+
+  const colorMapHeaderRef = useRef<HTMLParagraphElement>(null);
 
   const items = [
     { key: DrawMode.HIDE.toString(), label: "Hide values" },
@@ -51,9 +75,30 @@ export default function DrawModeSelector(propsInput: DrawModeSelectorProps): Rea
         "#000000",
       ],
     },
+    {
+      label: (
+        <p style={{ fontSize: "12px", margin: "0" }} ref={colorMapHeaderRef}>
+          Some text
+        </p>
+      ),
+      colors: [
+        "#ffffff",
+        "#f0f0f0",
+        "#dddddd",
+        "#c0c0c0",
+        "#9d9d9d",
+        "#808080",
+        "#525252",
+        "#393939",
+        "#191919",
+        "#000000",
+      ],
+    },
   ];
 
   const showColorPicker = props.selected === DrawMode.USE_COLOR;
+
+  const collapseItems: CollapseProps["items"] = [{ key: "1", label: "Use Color Ramp", children: <p>Some Text</p> }];
 
   return (
     <MainLayout style={{ margin: "5px 0" }}>
@@ -81,6 +126,14 @@ export default function DrawModeSelector(propsInput: DrawModeSelectorProps): Rea
           presets={presetColors}
           // onChange returns a different color type, so must convert from hex
           onChange={(_color, hex) => props.onChange(props.selected, new ThreeColor(hex as ColorRepresentation))}
+          panelRender={(panel, extra) => {
+            return (
+              <>
+                {panel}
+                <MiniCollapse items={collapseItems} ghost size="small" />
+              </>
+            );
+          }}
         />
       </HorizontalDiv>
     </MainLayout>
