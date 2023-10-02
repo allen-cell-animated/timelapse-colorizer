3a5943b37019b9e7c1c39297c1ec1f6145360afd
Peyton Lee <peyton.lee@alleninstitute.org>
2023-10-02 11:09:17 -0700

    fix: Fixed Not Implemented Error when running tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -8,6 +8,12 @@ import "vitest-canvas-mock";
 // https://github.com/vitest-dev/vitest/issues/3985
 window.URL.createObjectURL = vi.fn();
 
+// Fix for the following error:
+// `Error: Not implemented: window.computedStyle(elt, pseudoElt)`
+// https://github.com/nickcolley/jest-axe/issues/147 (not the source of error but a relevant workaround)
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
+
 afterEach(() => {
   cleanup();
 });
