38ce44ce5c8bd9b32dc0e7bf606150ccbae7b290
Peyton Lee <peyton.lee@alleninstitute.org>
2023-10-11 09:29:42 -0700

    doc: Updated comment about eslint globals

--- src/colorizer/recorders/Mp4VideoRecorder.ts
+++ src/colorizer/recorders/Mp4VideoRecorder.ts
@@ -3,7 +3,9 @@ import { ArrayBufferTarget, Muxer } from "mp4-muxer";
 import CanvasRecorder, { RecordingOptions, defaultRecordingOptions } from "./CanvasRecorder";
 import { sleep } from "../utils/timing_utils";
 
-// Eslint doesn't recognize the WebCodecs API yet
+// Eslint doesn't recognize the WebCodecs API yet.
+// This line prevents eslint from throwing errors like
+// 'VideoEncoder' is not defined (no-undef).
 /* global VideoEncoder, VideoFrame, VideoEncoderConfig*/
 
 export enum VideoBitrate {
