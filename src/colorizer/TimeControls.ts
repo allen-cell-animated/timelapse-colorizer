7eb820fd0fa157d160299dd419b7e7605dc5c565
Peyton Lee <peyton.lee@alleninstitute.org>
2024-01-04 15:27:59 -0800

    refactor: Removed redundant playback flag in TimeControls

--- src/colorizer/TimeControls.ts
+++ src/colorizer/TimeControls.ts
@@ -9,7 +9,6 @@ const NO_TIMER_ID = -1;
 export default class TimeControls {
   private timerId: number;
   private setFrameFn?: (frame: number) => Promise<void>;
-  private currentlyPlaying: boolean;
   private playbackFps: number;
 
   private canvas: ColorizeCanvas;
@@ -21,7 +20,6 @@ export default class TimeControls {
     this.timerId = NO_TIMER_ID;
     this.pauseCallbacks = [];
     this.playbackFps = playbackFps;
-    this.currentlyPlaying = false;
   }
 
   public setFrameCallback(fn: (frame: number) => Promise<void>): void {
@@ -38,18 +36,16 @@ export default class TimeControls {
   }
 
   private playTimeSeries(onNewFrameCallback: () => void): void {
-    if (this.currentlyPlaying) {
+    if (this.timerId !== NO_TIMER_ID) {
       return;
     }
 
-    this.currentlyPlaying = true;
-
-    // TODO: Fix this function so that it doesn't stop the slider from also operating?
+    // TODO: Fix this function so that it doesn't stop the slider from also operating
 
     // `lastFrameNum` is a parameter here because relying on `ColorizeCanvas.getCurrentFrame()` can
     // lead to race conditions that lead to frames getting loaded more than once.
     const loadNextFrame = async (lastFrameNum: number): Promise<void> => {
-      if (!this.currentlyPlaying) {
+      if (this.timerId === NO_TIMER_ID) {
         return;
       }
 
@@ -59,7 +55,7 @@ export default class TimeControls {
       if (nextFrame === lastFrameNum) {
         // Stop playing for single-frame datasets.
         // TODO: Disable time bar on the UI for datasets with only one frame.
-        this.currentlyPlaying = false;
+        this.timerId = NO_TIMER_ID;
         return;
       }
 
@@ -71,12 +67,18 @@ export default class TimeControls {
       const timeElapsed = endTime - startTime;
       onNewFrameCallback();
 
-      // TODO: Could add some sort of smoothing here to make the playback more consistent.
+      if (this.timerId === NO_TIMER_ID) {
+        // The timer was stopped while the frame was loading, so stop playback.
+        return;
+      }
+
       // Add additional delay, if needed, to maintain playback fps.
+      // TODO: Could add some sort of smoothing here to make the playback more consistent.
       const delayMs = Math.max(0, 1000 / this.playbackFps - timeElapsed);
       this.timerId = window.setTimeout(() => loadNextFrame(nextFrame), delayMs);
     };
-    loadNextFrame(this.canvas.getCurrentFrame());
+
+    this.timerId = window.setTimeout(() => loadNextFrame(this.canvas.getCurrentFrame()), 0);
   }
 
   public async handlePlayButtonClick(): Promise<void> {
@@ -89,9 +91,8 @@ export default class TimeControls {
   public handlePauseButtonClick(): void {
     if (this.timerId !== NO_TIMER_ID) {
       clearTimeout(this.timerId);
-      this.timerId = NO_TIMER_ID;
     }
-    this.currentlyPlaying = false;
+    this.timerId = NO_TIMER_ID;
     this.pauseCallbacks.forEach((callback) => callback());
   }
 
@@ -102,7 +103,7 @@ export default class TimeControls {
   }
 
   public isPlaying(): boolean {
-    return this.currentlyPlaying;
+    return this.timerId !== NO_TIMER_ID;
   }
 
   public addPauseListener(callback: () => void): void {
