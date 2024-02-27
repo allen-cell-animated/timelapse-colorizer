597b5e51ff42e757eaa36d060d1a9fbb09a72d36
Peyton Lee <peyton.lee@alleninstitute.org>
2024-02-27 10:46:41 -0800

    fix: Button styling on click

--- src/components/AccessibleDropdown.tsx
+++ src/components/AccessibleDropdown.tsx
@@ -5,22 +5,11 @@ import styled, { css } from "styled-components";
 
 import { DropdownSVG } from "../assets";
 
-const AntRecognizedButtonTypes = ["primary", "default", "dashed", "text", "link"];
-
-// TODO: Handle outlined styling for help button
-
-const convertToAntButtonType = (type: string): "primary" | "default" | "dashed" | "text" | "link" => {
-  if (AntRecognizedButtonTypes.includes(type)) {
-    return type as "primary" | "default" | "dashed" | "text" | "link";
-  }
-  return "default";
-};
-
 type AccessibleDropdownProps = {
   /** Text label to include with the dropdown. If null or undefined, hides the label. */
   label?: string | null;
 
-  /** Contents rendered inside the dropdown. By default, a  */
+  /** Contents to be rendered inside the dropdown. */
   dropdownContent: ReactElement;
   renderDropdownContent?: ((content: ReactElement) => ReactElement) | null;
   disabled?: boolean;
@@ -67,35 +56,39 @@ const MainButton = styled(Button)<{ $open: boolean; type: AccessibleDropdownProp
   // Override Antd width transition
   transition: all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1), width 0s;
 
-  // Override Ant styling for the default (outlined) button style
+  // Override Ant styling for the outlined button style
   // so it fills in solid when hovered
-  &.ant-btn-default:not(:disabled) {
-    border-color: var(--color-borders);
-    color: var(--color-text);
-  }
+  ${(props) => {
+    if (props.type === "outlined") {
+      return css`
+        &:not(:disabled) {
+          border-color: var(--color-borders);
+          color: var(--color-text);
+        }
 
-  &.ant-btn-default:not(:disabled):hover {
-    background-color: transparent;
-    border-color: var(--color-button);
-    color: var(--color-text); // Repeated to override color changes
-  }
+        &:not(:disabled):hover {
+          background-color: transparent;
+          border-color: var(--color-button);
+          color: var(--color-text); // Repeated to override color changes
+        }
 
-  &.ant-btn-default:not(:disabled):active {
-    background-color: transparent;
-    border-color: var(--color-button-active);
-    color: var(--color-text);
-  }
+        &:not(:disabled):active {
+          background-color: transparent;
+          border-color: var(--color-button-active);
+          color: var(--color-text);
+        }
+      `;
+    }
+    return;
+  }}
 
   // When the modal is opened ("pinned") by clicking on it, show an
   // extra active-style outline.
   ${(props) => {
     if (props.$open) {
       return css`
-        :not(:disabled) {
-          border-color: var(--color-button-active);
-        }
-        & .ant-btn-default:not(:disabled) {
-          border-color: var(--color-button);
+        &:not(:disabled) {
+          border-color: var(--color-button-active) !important;
         }
       `;
     }
@@ -130,39 +123,6 @@ const MainButtonContents = styled.div`
 /** Wrapper around the dropdown content. */
 const DropdownContentWrapper = styled.div`
   padding: 4px;
-`;
-
-/** A styled button inside the dropdown, which can be clicked to select an item. */
-const DropdownItem = styled(Button)<{ $selected: boolean }>`
-  text-align: left;
-  padding: 3px 12px;
-  border: 0px solid transparent;
-  border-radius: 4px;
-  overflow: hidden;
-
-  & span {
-    text-overflow: ellipsis;
-    overflow: hidden;
-    width: 100%;
-  }
-
-  ${(props) => {
-    // Important tags needed here to prevent ant default button styling from overriding
-    if (props.$selected) {
-      // If selected, override hover and focus styles
-      return css`
-        background-color: var(--color-dropdown-selected) !important;
-        color: var(--color-button) !important;
-      `;
-    }
-    // Otherwise, add grey backdrop when hovered or focused
-    return css`
-      &:hover,
-      &:focus {
-        background-color: var(--color-dropdown-hover) !important;
-      }
-    `;
-  }}
 `;
 
 /**
