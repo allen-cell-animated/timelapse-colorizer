cc76cc5d30d755ba6667aa711ae3f3fee9ef2b11
Peyton Lee <peyton.lee@alleninstitute.org>
2024-03-06 09:29:34 -0800

    feat: Improved error page formatting

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -11,14 +11,20 @@ const isErrorResponse = (error: unknown): error is ErrorResponse => {
 export default function ErrorPage(): ReactElement {
   const error = useRouteError() as unknown;
   let errorMessage = "";
+
   if (isErrorResponse(error)) {
     errorMessage = error.status + " " + error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else {
+    errorMessage = "Unknown error";
   }
 
   return (
-    <FlexColumnAlignCenter style={{ width: "100%", padding: "20px" }}>
+    <FlexColumnAlignCenter style={{ width: "100%", padding: "40px" }}>
       <h1>{errorMessage}</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, something went wrong.</p>
+      <br />
       <Link to="/">
         <Button type="primary">Return to homepage</Button>
       </Link>
