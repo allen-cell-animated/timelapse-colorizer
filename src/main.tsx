d80a7fc15ffb16417fd9a7c12e7afee6af8de7de
Peyton Lee <peyton.lee@alleninstitute.org>
2024-03-07 10:42:26 -0800

    test: Removed HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import AppStyle from "./components/AppStyle";
 import ErrorPage from "./routes/ErrorPage";
@@ -9,7 +9,7 @@ import Viewer from "./Viewer";
 // Set up react router
 // Use HashRouter for GitHub Pages support, so additional paths are routed to
 // the base app instead of trying to open pages that don't exist.
-const router = createHashRouter(
+const router = createBrowserRouter(
   [
     {
       path: "/*",
