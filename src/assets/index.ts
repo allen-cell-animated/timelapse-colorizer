fcb4f094ea7f64bcf53bac36275a93be54d39a6c
Peyton Lee <peyton.lee@alleninstitute.org>
2024-04-01 19:40:52 -0700

    fix: Linting

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,12 @@
-import AicsLogoAndNameSVGAsset from "../assets/AICS-logo-and-name.svg?react";
 import AicsLogoSVGAsset from "../assets/AICS-logo.svg?react";
+import AicsLogoAndNameSVGAsset from "../assets/AICS-logo-and-name.svg?react";
 import DropdownSVGAsset from "../assets/dropdown-arrow.svg?react";
 import ExportIconSVGAsset from "../assets/icon-export.svg?react";
 import SwitchIconSVGAsset from "../assets/icon-switch-arrows.svg?react";
 import NoImageSVGAsset from "../assets/no-image.svg?react";
 
-export const AicsLogoSVG = AicsLogoSVGAsset;
 export const AicsLogoAndNameSVG = AicsLogoAndNameSVGAsset;
+export const AicsLogoSVG = AicsLogoSVGAsset;
 export const DropdownSVG = DropdownSVGAsset;
 export const SwitchIconSVG = SwitchIconSVGAsset;
 export const NoImageSVG = NoImageSVGAsset;
