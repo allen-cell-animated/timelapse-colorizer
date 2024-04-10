617ce24234127d2010567c4c86547bb8b459c5d4
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-04-10 20:57:28 +0000

    Deploy preview for PR 321 🛫

--- pr-preview/pr-321/assets/BannerVideo-b87be867.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9335875b.js";const r="/nucmorph-colorizer/pr-preview/pr-321/assets/banner_video-783a41be.mp4";function t(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{t as default};
