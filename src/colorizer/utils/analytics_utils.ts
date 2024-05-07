123780de14f2d626be4368daf6d2f0121481d331
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-07 14:51:06 -0700

    feat: Added tracking for when features are changed

--- src/colorizer/utils/analytics_utils.ts
+++ src/colorizer/utils/analytics_utils.ts
@@ -1,4 +1,6 @@
-// gaslight typescript
+import { FeatureType } from "../Dataset";
+
+// Added by Google Tag Manager.
 declare global {
   interface Window {
     dataLayer: Record<string, any>[];
@@ -9,12 +11,15 @@ export const enum AnalyticsEvent {
   COLLECTION_LOAD = "collection_load",
   DATASET_LOAD = "dataset_load",
   EXPORT_COMPLETE = "export_complete",
+  FEATURE_SELECTED = "feature_selected",
 }
 
 export type AnalyticsEventPayload<T extends AnalyticsEvent> = {
   [AnalyticsEvent.COLLECTION_LOAD]: { collection_writer_version: string };
+  // Include number of elements in the dataset? Number of features?
   [AnalyticsEvent.DATASET_LOAD]: { dataset_writer_version: string };
   [AnalyticsEvent.EXPORT_COMPLETE]: { export_format: "mp4" | "png" };
+  [AnalyticsEvent.FEATURE_SELECTED]: { feature_type: FeatureType; feature_range: number };
 }[T];
 
 /**
