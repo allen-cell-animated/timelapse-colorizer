9f0f587e79e81c340fb768735ced41c95150a3cf
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-10 17:03:57 -0700

    feat: Added 404.html template file, edited vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "404": resolve(__dirname, '404.html')
+      }
+    }
+  },
   plugins: [svgr(), glsl(), react()],
 });
