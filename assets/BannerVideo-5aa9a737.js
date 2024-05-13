1299f8c556a00ea96abdfb7a3de5b3059520e633
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-13 10:01:48 -0700

    'New

---
+++ assets/BannerVideo-5aa9a737.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-9c40094d.js";import"./gh_routing-04cbc116.js";const o="/nucmorph-colorizer/assets/banner_video-783a41be.mp4";function n(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:o,type:"video/mp4"})})}export{n as default};
