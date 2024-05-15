3abe3c8382d6017cb6bb1cdd82cb5aca17a03b9d
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-15 16:13:42 -0700

    Revert "feat: Add app version constant"
    This reverts commit d1b22191742d1b96c99ee464215afb5d190f888b.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [svgr(), glsl(), react()],
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
-  }
 });
