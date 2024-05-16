1a55e45589a4d5bf571b0bfeee75c24b692bf945
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-16 10:56:08 -0700

    fix: typeCheck

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,10 @@
 declare const APP_VERSION: string;
 declare const BUILD_TIME_UTC: number;
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly npm_package_version: string;
+    }
+  }
+}
