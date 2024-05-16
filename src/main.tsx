716894a319079cbae07f5034739e48ba11eb0e57
Peyton Lee <peyton.lee@alleninstitute.org>
2024-05-16 10:13:01 -0700

    feat: Added print statement for basename and version number on startup

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import { ErrorPage, LandingPage } from "./routes";
 import AppStyle from "./components/AppStyle";
 import Viewer from "./Viewer";
 
+const basename = import.meta.env.BASE_URL;
+console.log(`nucmorph-colorizer Version ${APP_VERSION}`);
+console.log(`nucmorph-colorizer Basename ${basename}`);
+
 // Set up react router
 // Use HashRouter for GitHub Pages support, so additional paths are routed to
 // the base app instead of trying to open pages that don't exist.
