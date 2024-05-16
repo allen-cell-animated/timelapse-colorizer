9739e466e3d7f83bc6166f763f992795f56cc688
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-05-16 19:50:51 +0000

    Deploy preview for PR 342 🛫

---
+++ pr-preview/pr-342/assets/BannerVideo-7b9988b0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a3aab360.js";const r="/nucmorph-colorizer/pr-preview/pr-342/assets/banner_video-783a41be.mp4";function t(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{t as default};
