cce678c307fd4511b0361a9e716b46f4fb6b43b1
ShrimpCryptid <peytonalee@gmail.com>
2024-05-20 21:27:48 +0000

    Deploying to gh-pages from @ allen-cell-animated/nucmorph-colorizer@8d9a15821e1aca7cd2d4f1bbaee066ac9c37bbb9 🚀

--- assets/BannerVideo-70e1caac.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-f07fb8c0.js";import"./gh_routing-cdb305c8.js";const o="/nucmorph-colorizer/assets/banner_video-783a41be.mp4";function n(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:o,type:"video/mp4"})})}export{n as default};
