425398ca295f8c34fecb5e554976e682e285be70
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-05-21 22:55:28 +0000

    Deploy preview for PR 347 🛫

--- pr-preview/pr-347/assets/BannerVideo-df350dcd.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-8a36be45.js";import"./gh_routing-cdb305c8.js";const r="/nucmorph-colorizer/pr-preview/pr-347/assets/banner_video-783a41be.mp4";function n(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{n as default};
