3ca4e6febf2726f03e2a04c93e50bfcb8f5db41f
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-06-20 18:43:24 +0000

    Deploy preview for PR 385 🛫

---
+++ pr-preview/pr-385/assets/404-842e4c78.js
@@ -0,0 +1,1 @@
+import{e as t}from"./gh_routing-cdb305c8.js";window.onload=()=>{document.body.innerHTML=""};const o=window.location,e=new URL(o.toString()),n=t(e);o.replace(n);console.log("Redirecting to "+n.toString());
