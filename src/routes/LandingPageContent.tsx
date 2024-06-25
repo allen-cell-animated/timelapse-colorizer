b9a5a567b8c20fdafa12d40ac8e96d9e43a1b399
Peyton Lee <peyton.lee@alleninstitute.org>
2024-06-25 15:18:15 -0700

    refactor: Added links for datasets

--- src/routes/LandingPageContent.tsx
+++ src/routes/LandingPageContent.tsx
@@ -1,14 +1,13 @@
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Color } from "three";
 
-import { DrawMode, ThresholdType } from "../colorizer/types";
+import { ThresholdType } from "../colorizer/types";
 import { ProjectEntry } from "../types";
 
 export const landingPageContent: ProjectEntry[] = [
   {
-    name: "Segmented hiPSC FOV-nuclei timelapse and analysis datasets",
+    name: "Tracked hiPSC FOV-nuclei timelapse datasets",
     inReview: true,
     description: (
       <p>
@@ -34,114 +33,67 @@ export const landingPageContent: ProjectEntry[] = [
       {
         name: "Exploratory dataset",
         description: "All successful tracked nuclei, with all available features and filters",
-        loadParams: {},
+        loadParams: {
+          collection:
+            "https://allencell.s3.amazonaws.com/aics/nuc-morph-dataset/timelapse_feature_explorer_datasets/exploratory_dataset/collection.json",
+          time: 15,
+          thresholds: [
+            {
+              featureKey: "growth_outlier_filter",
+              type: ThresholdType.CATEGORICAL,
+              unit: "",
+              enabledCategories: [true, true],
+            },
+            {
+              featureKey: "baseline_colonies_dataset_filter",
+              type: ThresholdType.CATEGORICAL,
+              unit: "",
+              enabledCategories: [true, true],
+            },
+            {
+              featureKey: "fullinterphase_dataset_filter",
+              type: ThresholdType.CATEGORICAL,
+              unit: "",
+              enabledCategories: [true, true],
+            },
+            {
+              featureKey: "lineageannotated_dataset_filter",
+              type: ThresholdType.CATEGORICAL,
+              unit: "",
+              enabledCategories: [true, true],
+            },
+          ],
+        },
       },
       {
         name: "Baseline colony dataset",
-        description: "Nuclei tracked throughout colony growth (subset of exploratory analysis dataset)",
-        loadParams: {},
+        description:
+          "Minimally filtered tracked nuclei with quantitative single-timepoint features (subset of exploratory analysis dataset)",
+        loadParams: {
+          collection:
+            "https://allencell.s3.amazonaws.com/aics/nuc-morph-dataset/timelapse_feature_explorer_datasets/baseline_colonies_dataset/collection.json",
+          time: 15,
+        },
       },
       {
         name: "Full-interphase dataset",
         description:
           "Nuclei tracked throughout interphase (subset of baseline colony analysis dataset, with added growth trajectory features)",
-        loadParams: {},
+        loadParams: {
+          collection:
+            "https://allencell.s3.amazonaws.com/aics/nuc-morph-dataset/timelapse_feature_explorer_datasets/full-interphase_dataset/collection.json",
+          time: 15,
+        },
       },
       {
         name: "Lineage-annotated dataset",
         description:
           "Nuclei tracked across multiple generations (subset of full-interphase analysis dataset, with added lineage annotation)",
-        loadParams: {},
-      },
-    ],
-  },
-  {
-    name: "NucMorph dataset name which usually is a rather long name so may wrap to two lines",
-    description:
-      "Introductory explanatory text about the dataset(s) and anything a user should know before opening in app. This should ideally only be a couple of sentences.",
-    publicationLink: new URL("https://www.google.com"),
-    publicationName:
-      "This is the name of the associated publication that the user can click to open in a new tab (Publisher name, mm/dd/yyyy)",
-    loadParams: {
-      collection: "https://dev-aics-dtp-001.int.allencell.org/dan-data/colorizer/data/collection.json",
-      dataset: "Mama Bear",
-      feature: "volume",
-    },
-    inReview: true,
-  },
-  {
-    name: "This is a project name in the case of multiple datasets belonging to a single project/publication",
-    description:
-      "Introductory explanatory text about the dataset(s) and anything a user should know before opening in app. This should ideally only be a couple of sentences.",
-    datasets: [
-      {
-        name: "This is a dataset with a semi-long name",
-        description: "This is a short description about this particular dataset. 2 lines at most.",
         loadParams: {
-          collection: "https://dev-aics-dtp-001.int.allencell.org/dan-data/colorizer/data/collection.json",
-          dataset: "Mama Bear",
-          feature: "height",
-          range: [0.54, 9.452],
-          colorRampKey: "esri-mentone_beach",
-          time: 154,
+          collection:
+            "https://allencell.s3.amazonaws.com/aics/nuc-morph-dataset/timelapse_feature_explorer_datasets/lineage-annotated_dataset/collection.json",
+          time: 15,
         },
-      },
-      {
-        name: "This is a dataset with a semi-long name",
-        description: "This is a long description about this particular dataset. 2 lines at most.",
-        loadParams: {
-          collection: "https://dev-aics-dtp-001.int.allencell.org/dan-data/colorizer/data/collection.json",
-          dataset: "Goldilocks",
-          feature: "height",
-          range: [1.084, 8.156],
-          colorRampKey: "esri-blue_red_8",
-          time: 446,
-        },
-      },
-      {
-        name: "This is a dataset with a semi-long name",
-        description: "This is a short description about this particular dataset. 2 lines at most.",
-        loadParams: {
-          collection: "https://dev-aics-dtp-001.int.allencell.org/dan-data/colorizer/data/collection.json",
-          dataset: "Baby Bear",
-          feature: "height",
-          range: [0.54, 8.895],
-          colorRampKey: "esri-green_brown_1",
-          time: 397,
-          thresholds: [
-            {
-              featureKey: "volume",
-              type: ThresholdType.NUMERIC,
-              unit: "µm³",
-              min: 649.121,
-              max: 2457.944,
-            },
-          ],
-          config: {
-            outOfRangeDrawSettings: {
-              mode: DrawMode.USE_COLOR,
-              color: new Color("#858585"),
-            },
-          },
-        },
-      },
-    ],
-  },
-  {
-    name: "This is a project name in the case of multiple datasets belonging to a single project/publication",
-    description:
-      "Introductory explanatory text about the dataset(s) and anything a user should know before opening in app. This should ideally only be a couple of sentences.",
-    datasets: [
-      {
-        name: "This is a dataset with a semi-long name",
-        description: "This is a short description about this particular dataset. 2 lines at most.",
-        loadParams: {},
-      },
-      {
-        name: "This is a dataset with a semi-long name",
-        description:
-          "This is a long description about this particular dataset. 2 lines at most, but it has extra lines, so the entire section should wrap.",
-        loadParams: {},
       },
     ],
   },
