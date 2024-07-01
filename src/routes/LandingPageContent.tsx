9a635cf8393956bf2c10149199a00a7175fdaee7
Cameron Fraser <cameron.fraser@alleninstitute.org>
2024-07-01 09:08:58 -0700

    add biorxiv url

--- src/routes/LandingPageContent.tsx
+++ src/routes/LandingPageContent.tsx
@@ -15,7 +15,7 @@ export const landingPageContent: ProjectEntry[] = [
         features of nuclear shape, size and more. The exploratory dataset includes all tracked nuclei, with the baseline
         colonies, full-interphase, and lineage-annotated datasets as subsets of this dataset, analyzed in the study of
         nuclear growth{" "}
-        <a href="https://www.biorxiv.org/" rel="noopener noreferrer" target="_blank">
+        <a href="https://www.biorxiv.org/content/10.1101/2024.06.28.601071v1" rel="noopener noreferrer" target="_blank">
           {"Dixon et al. 2024"}
           <FontAwesomeIcon icon={faUpRightFromSquare} size="sm" style={{ marginBottom: "-1px", marginLeft: "3px" }} />
         </a>
