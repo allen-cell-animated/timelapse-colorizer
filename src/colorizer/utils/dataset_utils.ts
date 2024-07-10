1fd9ed17134a754e2e96e23e4a6435cfaaf58dd1
Peyton Lee <peyton.lee@alleninstitute.org>
2024-07-10 16:44:10 -0700

    Revert "Revert "refactor: Removed parquetjs""
    This reverts commit 78403119cf9ed97a171e6f53af42017e6d7e5627.

--- src/colorizer/utils/dataset_utils.ts
+++ src/colorizer/utils/dataset_utils.ts
@@ -64,6 +64,8 @@ type ManifestFileV1_0_0 = Omit<ManifestFileV0_0_0, "features" | "featureMetadata
     unit?: string;
     type?: string;
     categories?: string[];
+    min?: number;
+    max?: number;
   }[];
   /** Optional list of backdrop/overlay images. */
   backdrops?: { name: string; key: string; frames: string[] }[];
