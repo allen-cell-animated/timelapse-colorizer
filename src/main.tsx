0d89894800e6606733b2f1e312c91a89d996f1f5
Peyton Lee <peyton.lee@alleninstitute.org>
2024-07-24 17:18:55 -0700

    refactor: Disabled browser routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 
 import { getBuildDisplayDateString } from "./colorizer/utils/math_utils";
 import { ErrorPage, LandingPage } from "./routes";
@@ -9,16 +9,16 @@ import { decodeGitHubPagesUrl, isEncodedPathUrl, tryRemoveHashRouting } from "./
 import AppStyle from "./components/AppStyle";
 import Viewer from "./Viewer";
 
-// Decode URL path if it was encoded for GitHub pages or uses hash routing.
-const locationUrl = new URL(window.location.toString());
-if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
-  const decodedUrl = tryRemoveHashRouting(decodeGitHubPagesUrl(locationUrl));
-  const newRelativePath = decodedUrl.pathname + decodedUrl.search + decodedUrl.hash;
-  console.log("Redirecting to " + newRelativePath);
-  // Replaces the query string path with the original path now that the
-  // single-page app has loaded. This lets routing work as normal below.
-  window.history.replaceState(null, "", newRelativePath);
-}
+// // Decode URL path if it was encoded for GitHub pages or uses hash routing.
+// const locationUrl = new URL(window.location.toString());
+// if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
+//   const decodedUrl = tryRemoveHashRouting(decodeGitHubPagesUrl(locationUrl));
+//   const newRelativePath = decodedUrl.pathname + decodedUrl.search + decodedUrl.hash;
+//   console.log("Redirecting to " + newRelativePath);
+//   // Replaces the query string path with the original path now that the
+//   // single-page app has loaded. This lets routing work as normal below.
+//   window.history.replaceState(null, "", newRelativePath);
+// }
 
 const version = import.meta.env.VITE_APP_VERSION;
 const basename = import.meta.env.BASE_URL;
@@ -28,7 +28,7 @@ console.log(`Timelapse Feature Explorer - Basename ${basename}`);
 console.log(`Timelapse Feature Explorer - Last built ${getBuildDisplayDateString()}`);
 
 // Set up react router
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
@@ -40,8 +40,8 @@ const router = createBrowserRouter(
       element: <Viewer />,
       errorElement: <ErrorPage />,
     },
-  ],
-  { basename: import.meta.env.BASE_URL }
+  ]
+  // { basename: import.meta.env.BASE_URL }
 );
 
 // Render React component
