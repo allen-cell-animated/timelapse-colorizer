e4a8d14f25123979e364204529f4909c29fa0aff
Peyton Lee <peyton.lee@alleninstitute.org>
2024-08-08 11:03:34 -0700

    refactor: Moved JSON loading to worker as well, refactoring type handling

--- src/colorizer/loaders/UrlArrayLoader.ts
+++ src/colorizer/loaders/UrlArrayLoader.ts
@@ -4,38 +4,22 @@ import { DataTexture } from "three";
 import workerpool from "workerpool";
 
 import { FeatureArrayType, FeatureDataType, featureTypeSpecs } from "../types";
-import { nanToNull } from "../utils/data_utils";
 import { packDataTexture } from "../utils/texture_utils";
 
 import { ArraySource, IArrayLoader } from "./ILoader";
 
-type FeatureDataJson = {
-  data: number[] | boolean[];
-  min?: number;
-  max?: number;
-};
-
-const isBoolArray = (arr: number[] | boolean[]): arr is boolean[] => typeof arr[0] === "boolean";
-
 export class UrlArraySource implements ArraySource {
   array: number[];
-  isBool: boolean;
-  min?: number;
-  max?: number;
+  min: number;
+  max: number;
 
-  constructor(array: number[] | boolean[], min?: number | boolean, max?: number | boolean) {
-    if (isBoolArray(array)) {
-      this.array = array.map(Number);
-      this.isBool = true;
-    } else {
-      // Must store Infinity values internally because WebGL states that NaN behavior is undefined.
-      // This can cause shaders to not detect NaN, and operations like isnan() fail.
-      // On the UI, however, Infinity should be parsed as NaN for display.
-      this.array = array.map((val) => (val === null ? Infinity : val));
-      this.isBool = false;
-    }
-    this.min = typeof min === "boolean" ? Number(min) : min;
-    this.max = typeof max === "boolean" ? Number(max) : max;
+  constructor(array: number[] | Float32Array, min: number, max: number) {
+    // Must store Infinity values internally because WebGL states that NaN behavior is undefined.
+    // This can cause shaders to not detect NaN, and operations like isnan() fail.
+    // On the UI, however, Infinity should be parsed as NaN for display.
+    this.array = array.map((val) => (val === null ? Infinity : val));
+    this.min = min;
+    this.max = max;
   }
 
   getBuffer<T extends FeatureDataType>(type: T): FeatureArrayType[T] {
@@ -47,16 +31,10 @@ export class UrlArraySource implements ArraySource {
   }
 
   getMin(): number {
-    if (this.min === undefined) {
-      this.min = this.array.reduce((acc, val) => (val < acc ? val : acc));
-    }
     return this.min;
   }
 
   getMax(): number {
-    if (this.max === undefined) {
-      this.max = this.array.reduce((acc, val) => (val > acc ? val : acc));
-    }
     return this.max;
   }
 }
@@ -80,36 +58,19 @@ export default class UrlArrayLoader implements IArrayLoader {
    *   in JSON files or the calculated minimum value for Parquet files.
    * @param max Optional maximum value for the data. If defined, overrides the `max` field
    *   in JSON files or the calculated maximum value for Parquet files.
+   * @param type Optional data type for the returned array. Defaults to `FeatureDataType.F32`.
    * @throws Error if the file format is not supported (not JSON or Parquet).
    * @returns a URLArraySource object containing the loaded data.
    */
-  async load(url: string, min?: number, max?: number): Promise<UrlArraySource> {
-    if (url.endsWith(".json")) {
-      const response = await fetch(url);
-      const text = await response.text();
-      const { data, min: jsonMin, max: jsonMax }: FeatureDataJson = JSON.parse(nanToNull(text));
-      return new UrlArraySource(data, min ?? jsonMin, max ?? jsonMax);
-    } else if (url.endsWith(".parquet")) {
-      const { data, min: newMin, max: newMax } = await this.workerPool.exec("loadFromParquetUrl", [url, min, max]);
+  async load(
+    url: string,
+    min?: number,
+    max?: number,
+    type: FeatureDataType = FeatureDataType.F32
+  ): Promise<UrlArraySource> {
+    if (url.endsWith(".json") || url.endsWith(".parquet")) {
+      const { data, min: newMin, max: newMax } = await this.workerPool.exec("load", [url, type]);
       return new UrlArraySource(data, min ?? newMin, max ?? newMax);
-      // const result = await fetch(url);
-      // const arrayBuffer = await result.arrayBuffer();
-      // let data: number[] = [];
-      // let dataMin: number | undefined = undefined;
-      // let dataMax: number | undefined = undefined;
-      // await parquetRead({
-      //   file: arrayBuffer,
-      //   compressors,
-      //   onComplete: (loadedData: number[][]) => {
-      //     for (const row of loadedData) {
-      //       dataMin = dataMin === undefined ? row[0] : Math.min(dataMin, row[0]);
-      //       dataMax = dataMax === undefined ? row[0] : Math.max(dataMax, row[0]);
-      //       data.push(row[0]);
-      //     }
-      //     data = loadedData.map((row) => Number(row[0]));
-      //   },
-      // });
-      // return new UrlArraySource(data, min ?? dataMin, max ?? dataMax);
     } else {
       throw new Error(`Unsupported file format for URL array loader: ${url}`);
     }
--- src/colorizer/loaders/workers/urlLoadWorker.ts
+++ src/colorizer/loaders/workers/urlLoadWorker.ts
@@ -3,28 +3,76 @@ import { compressors } from "hyparquet-compressors";
 import workerpool from "workerpool";
 import Transfer from "workerpool/types/transfer";
 
-async function loadFromParquetUrl(url: string): Promise<Transfer> {
+import { FeatureDataType, featureTypeSpecs } from "../../types";
+import { nanToNull } from "../../utils/data_utils";
+
+const isBoolArray = (arr: number[] | boolean[]): arr is boolean[] => typeof arr[0] === "boolean";
+
+type FeatureDataJson = {
+  data: number[] | boolean[];
+  min?: number;
+  max?: number;
+};
+
+async function loadFromJsonUrl(url: string, type: FeatureDataType): Promise<Transfer> {
+  const result = await fetch(url);
+  const text = await result.text();
+  let { data: rawData, min, max }: FeatureDataJson = JSON.parse(nanToNull(text));
+
+  // Construct typed array from raw data so it can be transferred w/o copying to
+  // main thread
+  if (isBoolArray(rawData)) {
+    rawData = rawData.map(Number);
+  }
+  // Replace null values with Infinity due to WebGL not supporting NaN
+  for (let i = 0; i < rawData.length; i++) {
+    if (rawData[i] === null) {
+      rawData[i] = Infinity;
+    }
+  }
+  const data = new featureTypeSpecs[type].ArrayConstructor(rawData);
+
+  return new workerpool.Transfer({ min, max, data }, [data.buffer]);
+}
+
+async function loadFromParquetUrl(url: string, type: FeatureDataType): Promise<Transfer> {
   const result = await fetch(url);
   const arrayBuffer = await result.arrayBuffer();
-  let data: Float32Array = new Float32Array(0);
+  let data: Float32Array | Uint32Array | Uint8Array = new Float32Array(0);
   let dataMin: number | undefined = undefined;
   let dataMax: number | undefined = undefined;
+
   await parquetRead({
     file: arrayBuffer,
     compressors,
-    onComplete: (loadedData: number[][]) => {
-      const data = new Float32Array(loadedData.length);
-      for (let i = 0; i < loadedData.length; i++) {
-        const value = loadedData[i][0];
+    onComplete: (rawData: number[][]) => {
+      const data = new featureTypeSpecs[type].ArrayConstructor(rawData.flat().map(Number));
+
+      // Get min and max values for the data
+      for (let i = 0; i < data.length; i++) {
+        const value = Number(data);
         dataMin = dataMin === undefined ? value : Math.min(dataMin, value);
         dataMax = dataMax === undefined ? value : Math.max(dataMax, value);
-        data[i] = value;
       }
     },
   });
+
   return new workerpool.Transfer({ min: dataMin, max: dataMax, data }, [data.buffer]);
 }
 
+async function load(url: string, type: FeatureDataType): Promise<Transfer> {
+  if (url.endsWith(".json")) {
+    return loadFromJsonUrl(url, type);
+  } else if (url.endsWith(".parquet")) {
+    return loadFromParquetUrl(url, type);
+  } else {
+    throw new Error(`Unsupported file format: ${url}`);
+  }
+
+  // TODO: Also generate textures for the data on the worker thread too?
+  // Would need access to the underlying array buffer to transfer
+}
+
 workerpool.worker({
-  loadFromParquetUrl: loadFromParquetUrl,
+  load: load,
 });
