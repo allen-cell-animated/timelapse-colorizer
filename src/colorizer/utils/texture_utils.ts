52742b8b477cff740069051334db98e39d639770
Peyton Lee <peyton.lee@alleninstitute.org>
2024-08-08 15:31:11 -0700

    fix: Added support for generic number arrays when packing into square textures

--- src/colorizer/utils/texture_utils.ts
+++ src/colorizer/utils/texture_utils.ts
@@ -5,7 +5,7 @@ import { FeatureArrayType, FeatureDataType, featureTypeSpecs } from "../types";
 /**
  * Calculate the squarest possible texture that `data` can fit into and return a tuple of `[width, height]`
  */
-function getSquareDimensions<T extends FeatureDataType>(data: FeatureArrayType[T]): [number, number] {
+function getSquareDimensions<T extends FeatureDataType>(data: FeatureArrayType[T] | number[]): [number, number] {
   const width = Math.ceil(Math.sqrt(data.length));
   const height = Math.ceil(data.length / width);
 
@@ -28,15 +28,20 @@ function padToLength<T extends FeatureDataType>(
 }
 
 /** Pack a 1d array of data into the squarest 2d texture possible */
-export function packDataTexture<T extends FeatureDataType>(data: FeatureArrayType[T], type: T): DataTexture {
+export function packDataTexture<T extends FeatureDataType>(data: FeatureArrayType[T] | number[], type: T): DataTexture {
   const [width, height] = getSquareDimensions(data);
   const length = width * height;
+
+  // Convert array of regular numbers to typed arrays
+  if (!ArrayBuffer.isView(data)) {
+    data = new featureTypeSpecs[type].ArrayConstructor(data);
+  }
 
   const spec = featureTypeSpecs[type];
   const buffer = padToLength(data, type, 0, length);
 
-  // Convert all NaNs in the buffer to Infinity to avoid issues with WebGL.
-  // This assumes that the data is float-based.
+  // Convert all NaNs in the buffer to Infinity before texture conversion, as WebGL has undefined
+  // behavior for NaNs. This assumes that the data is float-based.
   for (let i = 0; i < buffer.length; i++) {
     if (Number.isNaN(buffer[i])) {
       buffer[i] = Infinity;
