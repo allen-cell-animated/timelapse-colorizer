b6a4f690a21421be007c12cf0923c50456f22bae
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-09-20 18:53:22 +0000

    Deploy preview for PR 441 🛫

---
+++ pr-preview/pr-441/assets/BannerVideo-2d0cb465.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-b241aa19.js";import"./gh_routing-cdb305c8.js";const r="/timelapse-colorizer/pr-preview/pr-441/assets/banner_video-783a41be.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
