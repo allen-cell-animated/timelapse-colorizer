65d560539829db059e52b1e63e7d1d179534f771
Peyton Lee <peyton.lee@alleninstitute.org>
2024-10-17 17:29:08 -0700

    feat: Matched popup notifications to alert banner styling

--- src/components/AppStyle.tsx
+++ src/components/AppStyle.tsx
@@ -29,10 +29,14 @@ const palette = {
   link: "#0094FF",
   linkDark: "#007FD6",
   warning: "#faad14",
-  successLight: "#b7eb8f",
-  errorLight: "#ffa39e",
-  infoLight: "#91d5ff",
-  warningLight: "#ffe58f",
+  successMedium: "#b7eb8f",
+  successLight: "#f6ffed",
+  errorMedium: "#ffa39e",
+  errorLight: "#fff2f0",
+  infoMedium: "#91d5ff",
+  infoLight: "#e6f4ff",
+  warningMedium: "#ffe58f",
+  warningLight: "#fffbe6",
 };
 
 // Note: Some advanced version of this could swap different theme objects, and
@@ -93,10 +97,16 @@ const theme = {
     },
     alert: {
       border: {
-        info: palette.infoLight,
-        warning: palette.warningLight,
+        info: palette.infoMedium,
+        warning: palette.warningMedium,
+        error: palette.errorMedium,
+        success: palette.successMedium,
+      },
+      fill: {
         error: palette.errorLight,
+        warning: palette.warningLight,
         success: palette.successLight,
+        info: palette.infoLight,
       },
     },
   },
@@ -191,6 +201,10 @@ const CssContainer = styled.div`
   --color-alert-warning-border: ${theme.color.alert.border.warning};
   --color-alert-error-border: ${theme.color.alert.border.error};
   --color-alert-success-border: ${theme.color.alert.border.success};
+  --color-alert-error-fill: ${theme.color.alert.fill.error};
+  --color-alert-warning-fill: ${theme.color.alert.fill.warning};
+  --color-alert-success-fill: ${theme.color.alert.fill.success};
+  --color-alert-info-fill: ${theme.color.alert.fill.info};
 
   /* Fonts */
   --default-font: ${theme.font.family};
--- src/utils/formatting.tsx
+++ src/utils/formatting.tsx
@@ -13,12 +13,12 @@ export const RenderedStringContainer = styled.div`
 `;
 
 /**
- * Renders a text string array as JSX elements, handling formatting for lists and paragraph text.
+ * Renders a text string array as JSX react elements, handling formatting for lists and paragraph text.
  * @param items List of string text items to render.
  * @param containerStyle optional CSS properties object that will be applied to the container div.
  * @returns A list of one or more text elements, wrapped in a styled div.
  * String items will be returned as one of the following:
- * - If it starts with ` - `, the item will be rendered as a `li` element. Groups of `li` elements will be wrapped in a `ul` element.
+ * - If it starts with "- ", the item will be rendered as a `li` element. Groups of `li` elements will be wrapped in a `ul` element.
  * - Otherwise, the item is wrapped in a `p` element.
  */
 export function renderStringArrayAsJsx(items: string[] | string | undefined): ReactNode {
@@ -33,9 +33,9 @@ export function renderStringArrayAsJsx(items: string[] | string | undefined): Re
   let listElements: ReactNode[] = [];
 
   for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (item.startsWith(" - ")) {
-      listElements.push(<li key={listElements.length}>{item.substring(3)}</li>);
+    const item = items[i].trim();
+    if (item.startsWith("- ")) {
+      listElements.push(<li key={listElements.length}>{item.substring(2)}</li>);
       continue;
     } else {
       if (listElements.length > 0) {
