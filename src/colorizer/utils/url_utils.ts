baaa3ffa0f537db1dd7483fbcdcda14e7a291e7a
Peyton Lee <peyton.lee@alleninstitute.org>
2024-10-21 11:46:21 -0700

    refactor: Moved worker logic in `UrlArrayLoader` to `SharedWorkerPool` for extensibility

--- src/colorizer/utils/url_utils.ts
+++ src/colorizer/utils/url_utils.ts
@@ -22,8 +22,8 @@ import {
   ThresholdType,
   ViewerConfig,
 } from "../types";
+import { nanToNull } from "./data_load_utils";
 import { AnyManifestFile } from "./dataset_utils";
-import { nanToNull } from "./json_utils";
 import { numberToStringDecimal } from "./math_utils";
 
 enum UrlParam {
