ca92724b1ea63a4446aa4ebaf34542b9cdb219b0
Peyton Lee <peyton.lee@alleninstitute.org>
2024-10-22 10:14:48 -0700

    refactor: Data arrays are no longer copied in sanitization step

--- src/colorizer/utils/correlation.ts
+++ src/colorizer/utils/correlation.ts
@@ -1,30 +1,3 @@
-const sanitizeNumericDataArrays = (
-  xData: Float32Array,
-  yData: Float32Array
-): { xData: Float32Array; yData: Float32Array } => {
-  //   const xRet = new Float32Array(xData.length);
-  //   const yRet = new Float32Array(yData.length);
-  //   let j = 0;
-  //   for (let i = 0; i < xData.length; i++) {
-  //     if (Number.isFinite(xData[i]) && Number.isFinite(yData[i])) {
-  //       xRet[j] = xData[i];
-  //       yRet[j] = yData[i];
-  //       j = j + 1;
-  //     }
-  //   }
-  //   return { xData: xRet.slice(0, j), yData: yRet.slice(0, j) };
-
-  // Boolean array, true if both x and y are not NaN/infinity
-  const isFiniteLut = Array.from(Array(xData.length)).map(
-    (_, i) => Number.isFinite(xData[i]) && Number.isFinite(yData[i])
-  );
-
-  return {
-    xData: xData.filter((_, i) => isFiniteLut[i]),
-    yData: yData.filter((_, i) => isFiniteLut[i]),
-  };
-};
-
 // Source: https://memory.psych.mun.ca/tech/js/correlation.shtml
 // Takes two arrays of numbers
 function pearson(x: Float32Array, y: Float32Array): number {
@@ -46,11 +19,16 @@ function pearson(x: Float32Array, y: Float32Array): number {
   const xy = new Float32Array(shortestArrayLength);
   const x2 = new Float32Array(shortestArrayLength);
   const y2 = new Float32Array(shortestArrayLength);
+  let totalFiniteElements = 0;
 
   for (let i = 0; i < shortestArrayLength; i++) {
+    if (!Number.isFinite(x[i]) || !Number.isFinite(y[i])) {
+      continue;
+    }
     xy[i] = x[i] * y[i];
     x2[i] = x[i] * x[i];
     y2[i] = y[i] * y[i];
+    totalFiniteElements++;
   }
 
   var sumx = 0;
@@ -60,6 +38,9 @@ function pearson(x: Float32Array, y: Float32Array): number {
   var sumy2 = 0;
 
   for (let i = 0; i < shortestArrayLength; i++) {
+    if (!Number.isFinite(x[i]) || !Number.isFinite(y[i])) {
+      continue;
+    }
     sumx += x[i];
     sumy += y[i];
     sumxy += xy[i];
@@ -67,9 +48,9 @@ function pearson(x: Float32Array, y: Float32Array): number {
     sumy2 += y2[i];
   }
 
-  var step1 = shortestArrayLength * sumxy - sumx * sumy;
-  var step2 = shortestArrayLength * sumx2 - sumx * sumx;
-  var step3 = shortestArrayLength * sumy2 - sumy * sumy;
+  var step1 = totalFiniteElements * sumxy - sumx * sumy;
+  var step2 = totalFiniteElements * sumx2 - sumx * sumx;
+  var step3 = totalFiniteElements * sumy2 - sumy * sumy;
   var step4 = Math.sqrt(step2 * step3);
   var answer = step1 / step4;
 
@@ -94,8 +75,8 @@ export function computeCorrelations(featureData: Float32Array[]): number[][] {
       // get list of values for colx and coly
       const xvals = featureData[colx];
       const yvals = featureData[coly];
-      const { xData, yData } = sanitizeNumericDataArrays(xvals, yvals);
-      const val = pearson(xData, yData);
+      // const { xData, yData } = sanitizeNumericDataArrays(xvals, yvals);
+      const val = pearson(xvals, yvals);
       row.push(val);
     }
     out.push(row);
