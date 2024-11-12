b35123f9bc97fe026b268e95a8b31c9f1831c079
Peyton Lee <peyton.lee@alleninstitute.org>
2024-11-12 10:38:56 -0800

    feat: Added constant for internal builds, indicators in console, version info, and header

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { getBuildDisplayDateString } from "./colorizer/utils/math_utils";
+import { INTERNAL_BUILD } from "./constants";
 import { ErrorPage, LandingPage } from "./routes";
 import { decodeGitHubPagesUrl, isEncodedPathUrl, tryRemoveHashRouting } from "./utils/gh_routing";
 
@@ -22,12 +23,11 @@ if (locationUrl.hash !== "" || isEncodedPathUrl(locationUrl)) {
 
 const version = import.meta.env.VITE_APP_VERSION;
 const basename = import.meta.env.BASE_URL;
-const internalBuild = import.meta.env.VITE_INTERNAL_BUILD === "true";
 
 console.log(`Timelapse Feature Explorer - Version ${version}`);
 console.log(`Timelapse Feature Explorer - Basename ${basename}`);
 console.log(`Timelapse Feature Explorer - Last built ${getBuildDisplayDateString()}`);
-console.log(`Timelapse Feature Explorer - Internal build: ${internalBuild}`);
+INTERNAL_BUILD && console.log("Timelapse Feature Explorer - --INTERNAL BUILD--");
 
 // Set up react router
 const router = createBrowserRouter(
