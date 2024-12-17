75eef33804f897127a4cbd98c315427062ed985f
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2024-12-17 23:21:16 +0000

    Deploy preview for PR 499 🛫

--- pr-preview-internal/pr-499/assets/BannerVideo-a1be85b1.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-e5a9b67d.js";import"./gh_routing-cdb305c8.js";const r="/timelapse-colorizer/pr-preview-internal/pr-499/assets/banner_video-783a41be.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
