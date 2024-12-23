e7294480697c011eaad121549b2a98c8c46667f6
Peyton Lee <peyton.lee@alleninstitute.org>
2024-12-23 15:37:37 -0800

    refactor: Update comments

--- src/utils/file_io.ts
+++ src/utils/file_io.ts
@@ -1,15 +1,14 @@
 /**
- * Initiates a browser download of a file from a URL.
+ * Initiates a browser download of a file using a temporary anchor element.
  * @param filename The default filename to save the file as.
- * @param url The `href` attribute of the download link. This can either be a data
- * URL or
+ * @param href The `href` attribute of the download link.
  */
-export function download(filename: string, url: string): void {
+export function download(filename: string, href: string): void {
   // TODO: Add option to show save file picker? https://developer.mozilla.org/en-US/docs/Web/API/Window/showSaveFilePicker
   const anchor = document.createElement("a");
   document.body.appendChild(anchor);
 
-  anchor.href = url;
+  anchor.href = href;
   anchor.download = filename;
   anchor.click();
 
