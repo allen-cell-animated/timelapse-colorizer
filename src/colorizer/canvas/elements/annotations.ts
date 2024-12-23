936a876482f4c1af1d6eed09896fb91487607ecd
Peyton Lee <peyton.lee@alleninstitute.org>
2024-12-23 10:12:11 -0800

    feat: Updated comments, added zoom scaling on markers

--- src/colorizer/canvas/elements/annotations.ts
+++ src/colorizer/canvas/elements/annotations.ts
@@ -19,12 +19,19 @@ export type AnnotationStyle = {
   markerSizePx: number;
   borderColor: string;
   additionalItemsOffsetPx: number;
+  /** Percentage, as a number from 0 to 1, of how much to scale the annotation
+   * size with the zoom level. 0 means no scaling (the marker size is fixed in
+   * onscreen pixels), 1 means the annotation markers will scale linearly with
+   * the zoom level (at 2x zoom the markers will be 2x bigger).
+   */
+  scaleWithZoomPct: number;
 };
 
 export const defaultAnnotationStyle = {
   markerSizePx: 5,
   borderColor: "white",
   additionalItemsOffsetPx: 2,
+  scaleWithZoomPct: 0.25,
 };
 
 function drawAnnotation(
@@ -53,22 +60,28 @@ function drawAnnotation(
   pos.add(params.canvasSize.clone().multiplyScalar(0.5)); // Move origin to top left corner
 
   const renderPos = new Vector2(pos.x + origin.x, pos.y + origin.y);
-
-  ctx.fillStyle = "#" + labelData.color.getHexString();
   ctx.strokeStyle = style.borderColor;
+
+  // Scale icons by the zoom level.
+  const zoomScale = Math.max(params.frameToCanvasCoordinates.x, params.frameToCanvasCoordinates.y);
+  const dampenedZoomScale = zoomScale * style.scaleWithZoomPct + (1 - style.scaleWithZoomPct);
+  const scaledMarkerSizePx = style.markerSizePx * dampenedZoomScale;
 
   // Render an extra outline if multiple labels are present.
   if (labelIdx.length > 1) {
-    const offsetRenderPos = renderPos.clone().addScalar(style.additionalItemsOffsetPx);
+    ctx.fillStyle = "#" + params.labelData[labelIdx[1]].color.getHexString();
+    const offsetRenderPos = renderPos.clone().addScalar(style.additionalItemsOffsetPx * dampenedZoomScale);
     ctx.beginPath();
-    ctx.arc(offsetRenderPos.x, offsetRenderPos.y, style.markerSizePx, 0, 2 * Math.PI);
+    ctx.arc(offsetRenderPos.x, offsetRenderPos.y, scaledMarkerSizePx, 0, 2 * Math.PI);
     ctx.closePath();
+    ctx.fill();
     ctx.stroke();
   }
 
   // Draw the marker at the new position.
+  ctx.fillStyle = "#" + labelData.color.getHexString();
   ctx.beginPath();
-  ctx.arc(renderPos.x, renderPos.y, style.markerSizePx, 0, 2 * Math.PI);
+  ctx.arc(renderPos.x, renderPos.y, scaledMarkerSizePx, 0, 2 * Math.PI);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
@@ -88,7 +101,8 @@ export function getAnnotationRenderer(
     render: (origin: Vector2) => {
       const currentLabelToIds = params.timeToLabelIds.get(params.frame) || {};
 
-      // Remap from ids to labels.
+      // Remap from labels->ids to ids->labels. Adjust ordering to render the
+      // currently selected label first if there is one.
       const idsToLabels = new Map<number, number[]>();
       for (const labelIdString in currentLabelToIds) {
         const labelId = parseInt(labelIdString, 10);
@@ -106,8 +120,8 @@ export function getAnnotationRenderer(
         }
       }
 
-      for (const [id, labelIdx] of idsToLabels) {
-        drawAnnotation(origin, ctx, params, style, id, labelIdx);
+      for (const [id, labelIdxs] of idsToLabels) {
+        drawAnnotation(origin, ctx, params, style, id, labelIdxs);
       }
     },
   };
