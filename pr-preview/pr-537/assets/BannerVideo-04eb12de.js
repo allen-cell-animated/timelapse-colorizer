2e1f66ec0b0cbf4527b0dff3688bb291419214c3
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2025-01-30 22:22:55 +0000

    Deploy preview for PR 537 🛫

--- pr-preview/pr-537/assets/BannerVideo-04eb12de.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-9bc606f2.js";import"./gh_routing-cdb305c8.js";const r="/timelapse-colorizer/pr-preview/pr-537/assets/banner_video-783a41be.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
