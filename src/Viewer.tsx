1774aad17a057595dbf387c26abc72abde9add2f
Peyton Lee <peyton.lee@alleninstitute.org>
2025-02-07 10:30:09 -0800

    feat: Holding shift now selects time range

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -42,6 +42,7 @@ import {
 } from "./colorizer";
 import { AnalyticsEvent, triggerAnalyticsEvent } from "./colorizer/utils/analytics";
 import { getColorMap, getInRangeLUT, thresholdMatchFinder, validateThresholds } from "./colorizer/utils/data_utils";
+import { useShortcutKey } from "./colorizer/utils/hooks";
 import {
   useAnnotations,
   useConstructor,
@@ -226,6 +227,7 @@ function Viewer(): ReactElement {
   const [findTrackInput, setFindTrackInput] = useState("");
   const [lastValidHoveredId, setLastValidHoveredId] = useState<number>(-1);
   const [showObjectHoverInfo, setShowObjectHoverInfo] = useState(false);
+  const isShiftPressed = useShortcutKey("Shift");
 
   // EVENT LISTENERS ////////////////////////////////////////////////////////
 
@@ -845,15 +847,10 @@ function Viewer(): ReactElement {
       if (dataset && track) {
         const id = track.getIdAtTime(currentFrame);
         // TODO: Check for shift key held
-        annotationState.handleAnnotationClick(dataset, id, false);
+        annotationState.handleAnnotationClick(dataset, id, isShiftPressed);
       }
     },
-    [
-      annotationState.isAnnotationModeEnabled,
-      annotationState.selectionMode,
-      annotationState.currentLabelIdx,
-      currentFrame,
-    ]
+    [annotationState.handleAnnotationClick, isShiftPressed, currentFrame]
   );
 
   // RECORDING CONTROLS ////////////////////////////////////////////////////
