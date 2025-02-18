77599b7f3113528041dc467476d7e90be38af600
Peyton Lee <peyton.lee@alleninstitute.org>
2025-02-18 13:15:55 -0800

    fix: Fixed compatibility issues with hot module reload

--- src/colorizer/state/ViewerState.ts
+++ src/colorizer/state/ViewerState.ts
@@ -5,7 +5,6 @@ import { BackdropSlice, createBackdropSlice } from "./slices/backdrop_slice";
 import { CollectionSlice, createCollectionSlice } from "./slices/collection_slice";
 import { ColorRampSlice, createColorRampSlice } from "./slices/color_ramp_slice";
 import { createDatasetSlice, DatasetSlice } from "./slices/dataset_slice";
-import { zustandHmrFix } from "./utils/store_utils";
 
 export type ViewerState = CollectionSlice & DatasetSlice & BackdropSlice & ColorRampSlice;
 
@@ -19,4 +18,26 @@ export const viewerStateStoreCreator: StateCreator<ViewerState> = (...a) => ({
 // TODO: Add documentation on usage here
 export const useViewerStateStore = create<ViewerState>()(viewerStateStoreCreator);
 
-zustandHmrFix("ViewerStateStore", useViewerStateStore);
+// Adds compatibility with hot module reloading.
+// Adapted from https://github.com/pmndrs/zustand/discussions/827#discussioncomment-9843290
+declare global {
+  interface Window {
+    __store: ViewerState;
+  }
+}
+
+if (import.meta.hot) {
+  useViewerStateStore.subscribe((state) => {
+    if (typeof window !== "undefined") {
+      window.__store = state;
+    }
+  });
+  import.meta.hot!.accept((newModule) => {
+    if (!newModule) return;
+    const newStore = newModule.useViewerStateStore;
+    if (!newStore) return;
+    if (window.__store) {
+      newStore.setState(window.__store, true);
+    }
+  });
+}
--- src/colorizer/state/utils/store_utils.ts
+++ src/colorizer/state/utils/store_utils.ts
@@ -1,4 +1,3 @@
-import { UseBoundStore } from "zustand";
 import { shallow } from "zustand/shallow";
 
 /**
@@ -41,22 +40,3 @@ export function computed<const TDeps extends readonly unknown[] = unknown[], TRe
     return cachedResult;
   };
 }
-
-export const zustandHmrFix = (name: string, useStore: UseBoundStore<any>) => {
-  if (import.meta.hot) {
-    const state = import.meta.hot!.data[name];
-    if (state) {
-      useStore.setState(import.meta.hot!.data[name]);
-    }
-    useStore.subscribe((state: any) => {
-      import.meta.hot!.data[name] = state;
-    });
-    import.meta.hot!.accept((newModule) => {
-      if (newModule) {
-        console.log("🔁  Accepting updated module: ", newModule);
-        console.log("Overriding state with", import.meta.hot!.data[name]);
-        useStore.setState(import.meta.hot!.data[name]);
-      }
-    });
-  }
-};
