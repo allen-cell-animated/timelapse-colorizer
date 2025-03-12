314500b02c8a898795a670ebbbf40682a0e8d847
Peyton Lee <peyton.lee@alleninstitute.org>
2025-03-12 16:18:04 -0700

    feat: Added serialization/deserialization to dataset and color ramp slices

---
+++ src/state/utils/store_io.ts
@@ -0,0 +1,37 @@
+import {
+  loadColorRampSliceFromParams,
+  loadDatasetSliceFromParams,
+  serializeColorRampSlice,
+  serializeDatasetSlice,
+} from "../slices";
+import { SerializedStoreData, Store } from "../types";
+
+import { ViewerState } from "../ViewerState";
+
+// SERIALIZATION /////////////////////////////////////////////////////////////////////////
+
+export const serializeViewerStateStore = (store: Store<ViewerState>): Partial<SerializedStoreData> => {
+  return {
+    ...serializeDatasetSlice(store),
+    ...serializeColorRampSlice(store),
+  };
+};
+
+export const serializedStoreDataToUrl = (data: SerializedStoreData): string => {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(data)) {
+    params.set(key, value);
+  }
+  return params.toString();
+};
+
+// DESERIALIZATION ///////////////////////////////////////////////////////////////////////
+
+export const loadViewerStateFromParams = async (store: Store<ViewerState>, params: URLSearchParams): Promise<void> => {
+  // Load the rest of the state. This MUST happen after the dataset is loaded into the store.
+  // NOTE: Ordering is important here, because of slice dependencies.
+  loadDatasetSliceFromParams(store, params);
+  // Other slices can be added here
+  // Ramp MUST be loaded last because it updates whenever thresholds or feature changes
+  loadColorRampSliceFromParams(store, params);
+};
