90b59ebcd1ba977745a6e3653038336d04b788ea
Peyton Lee <peyton.lee@alleninstitute.org>
2025-03-14 16:40:03 -0700

    feat: Added unit tests for threshold slice, refactored url_utils tests

--- tests/state/ViewerState/threshold_slice.test.ts
+++ tests/state/ViewerState/threshold_slice.test.ts
@@ -3,6 +3,8 @@ import { describe, expect, it } from "vitest";
 
 import { FeatureThreshold, ThresholdType } from "../../../src/colorizer";
 import { validateThresholds } from "../../../src/colorizer/utils/data_utils";
+import { UrlParam } from "../../../src/colorizer/utils/url_utils";
+import { loadThresholdSliceFromParams, serializeThresholdSlice } from "../../../src/state/slices";
 import { MOCK_DATASET, MockFeatureKeys } from "./constants";
 import { setDatasetAsync } from "./utils";
 
@@ -25,6 +27,18 @@ const OUTDATED_THRESHOLDS: FeatureThreshold[] = [
 const VALIDATED_THRESHOLDS = validateThresholds(MOCK_DATASET, OUTDATED_THRESHOLDS);
 const EXPECTED_IN_RANGE_LUT = new Uint8Array([0, 0, 0, 1, 1, 1, 1, 0, 0]);
 const EXPECTED_IN_RANGE_LUT_NO_THRESHOLD = new Uint8Array([1, 1, 1, 1, 1, 1, 1, 1, 1]);
+
+const DEFAULT_THRESHOLDS: FeatureThreshold[] = [
+  { featureKey: MockFeatureKeys.FEATURE1, unit: "meters", type: ThresholdType.NUMERIC, min: 0, max: 1 },
+  { featureKey: MockFeatureKeys.FEATURE2, unit: "(m)", type: ThresholdType.NUMERIC, min: 24, max: 60 },
+  {
+    featureKey: MockFeatureKeys.FEATURE3,
+    unit: "",
+    type: ThresholdType.CATEGORICAL,
+    enabledCategories: [true, true, true, false, false, false, true, true, true, false, false, false],
+  },
+];
+const DEFAULT_THRESHOLDS_SERIALIZED = "feature1:meters:0:1,feature2:(m):24:60,feature3::1c7";
 
 describe("ThresholdSlice", () => {
   describe("setThresholds", () => {
@@ -97,6 +111,31 @@ describe("ThresholdSlice", () => {
       expect(result.current.inRangeLUT).to.deep.equal(new Uint8Array(0));
       await setDatasetAsync(result, MOCK_DATASET);
       expect(result.current.inRangeLUT).to.deep.equal(EXPECTED_IN_RANGE_LUT);
+    });
+  });
+
+  describe("serializeThresholdSlice", () => {
+    it("serializes thresholds", () => {
+      const { result } = renderHook(() => useViewerStateStore());
+      act(() => {
+        result.current.setThresholds(DEFAULT_THRESHOLDS);
+      });
+      expect(result.current.thresholds).to.deep.equal(DEFAULT_THRESHOLDS);
+      const serialized = serializeThresholdSlice(result.current);
+      expect(serialized).to.deep.equal({ [UrlParam.THRESHOLDS]: DEFAULT_THRESHOLDS_SERIALIZED });
+    });
+  });
+
+  describe("loadThresholdSliceFromParams", () => {
+    it("deserializes thresholds", () => {
+      const { result } = renderHook(() => useViewerStateStore());
+      const params = new URLSearchParams();
+      params.set(UrlParam.THRESHOLDS, DEFAULT_THRESHOLDS_SERIALIZED);
+      act(() => {
+        result.current.setThresholds([]);
+        loadThresholdSliceFromParams(result.current, params);
+      });
+      expect(result.current.thresholds).to.deep.equal(DEFAULT_THRESHOLDS);
     });
   });
 });
