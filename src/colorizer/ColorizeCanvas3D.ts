50622d3724de3909692b4db1d804f9c497c4d07d
Peyton Lee <peyton.lee@alleninstitute.org>
2025-04-03 16:31:13 -0700

    fix: Fixed ColorizeCanvas3D API

--- src/colorizer/ColorizeCanvas3D.ts
+++ src/colorizer/ColorizeCanvas3D.ts
@@ -78,7 +78,7 @@ export class ColorizeCanvas3D implements IRenderCanvas {
     return this.canvasResolution.clone();
   }
 
-  getScaleInfo(): CanvasScaleInfo {
+  get scaleInfo(): CanvasScaleInfo {
     return {
       type: CanvasType.CANVAS_3D,
     };
@@ -154,9 +154,9 @@ export class ColorizeCanvas3D implements IRenderCanvas {
       this.pendingFrame = -1;
       return Promise.resolve({
         frame: this.currentFrame,
-        isFrameLoaded: true,
+        frameError: false,
         backdropKey: null,
-        isBackdropLoaded: true,
+        backdropError: false,
       });
     }
     if (requestedFrame === this.pendingFrame && this.pendingVolumePromise) {
@@ -174,11 +174,11 @@ export class ColorizeCanvas3D implements IRenderCanvas {
       this.render(true);
       this.currentFrame = requestedFrame;
       this.pendingFrame = -1;
-      const result = {
+      const result: FrameLoadResult = {
         frame: requestedFrame,
-        isFrameLoaded: true,
+        frameError: false,
         backdropKey: null,
-        isBackdropLoaded: true,
+        backdropError: false,
       };
       this.onLoadFrameCallback(result);
       return result;
