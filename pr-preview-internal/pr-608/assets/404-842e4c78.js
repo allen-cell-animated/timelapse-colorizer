39f54e319a1b5d26c23c24b56dcdbcbf8f4f8c62
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2025-04-03 17:18:53 +0000

    Remove preview for PR 608 🛬

--- pr-preview-internal/pr-608/assets/404-842e4c78.js
+++
@@ -1,1 +0,0 @@
-import{e as t}from"./gh_routing-cdb305c8.js";window.onload=()=>{document.body.innerHTML=""};const o=window.location,e=new URL(o.toString()),n=t(e);o.replace(n);console.log("Redirecting to "+n.toString());
