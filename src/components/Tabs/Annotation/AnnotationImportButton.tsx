166a6610b2c6e98b4f2c7717a2e7422e905f88c4
Peyton Lee <peyton.lee@alleninstitute.org>
2025-05-20 19:48:11 -0700

    refactor: Renamed "label" to "annotation" in annotation UI

--- src/components/Tabs/Annotation/AnnotationImportButton.tsx
+++ src/components/Tabs/Annotation/AnnotationImportButton.tsx
@@ -158,8 +158,8 @@ export default function AnnotationImportButton(props: AnnotationImportButtonProp
                         <FlexColumn>
                           <p style={{ margin: 0 }}>Merge matching annotations</p>
                           <p style={{ margin: 0 }}>
-                            Matching labels will be merged, all other labels will be appended. If there are per-object
-                            conflicts, the imported CSV overrides existing annotations.
+                            Matching annotations will be merged, all other annotations will be appended. If there are
+                            per-object conflicts, the imported CSV overrides existing data.
                           </p>
                         </FlexColumn>
                       ) : (
