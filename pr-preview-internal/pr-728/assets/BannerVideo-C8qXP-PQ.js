d2c56540cbf20b7510107ee13723c1e8a718b6e8
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2025-07-28 18:48:17 +0000

    Remove preview for PR 728 🛬

--- pr-preview-internal/pr-728/assets/BannerVideo-C8qXP-PQ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-Dd8uS1h6.js";import"./gh_routing-Ccc8RuuD.js";const r="/timelapse-colorizer/pr-preview-internal/pr-728/assets/banner_video-8h8QZ5tC.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
