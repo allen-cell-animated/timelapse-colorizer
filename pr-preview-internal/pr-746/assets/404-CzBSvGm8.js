9fabd62167c0f7a637b9b50fdbac7fdc68524d21
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2025-08-21 22:53:45 +0000

    Deploy preview for PR 746 🛫

---
+++ pr-preview-internal/pr-746/assets/404-CzBSvGm8.js
@@ -0,0 +1,1 @@
+import{e as t}from"./gh_routing-Ccc8RuuD.js";window.onload=()=>{document.body.innerHTML=""};const o=window.location,e=new URL(o.toString()),n=t(e);o.replace(n);console.log("Redirecting to "+n.toString());
