a185cd7b2261444eb70c2ce17747d285912abc96
Peyton Lee <peyton.lee@alleninstitute.org>
2025-08-27 11:14:22 -0700

    refactor: Moved inline progress indicator into a separate component
    (cherry picked from commit 4cd2e6ab4d34649dddbc2fdbb4a323dbbd5310ed)

---
+++ src/components/Feedback/StyledInlineProgress.tsx
@@ -0,0 +1,42 @@
+import { Progress } from "antd";
+import Tooltip from "antd/es/tooltip";
+import React, { ReactElement, useContext } from "react";
+
+import { TOOLTIP_TRIGGER } from "../../constants";
+
+import { AppThemeContext } from "../AppStyle";
+
+type StyledInlineProgressProps = {
+  percent: number;
+  error?: boolean;
+};
+
+const defaultProps: Partial<StyledInlineProgressProps> = {
+  error: false,
+};
+
+export default function StyledInlineProgress(inputProps: StyledInlineProgressProps): ReactElement {
+  const props = { ...defaultProps, ...inputProps };
+  const theme = useContext(AppThemeContext);
+
+  let progressBarColor = theme.color.theme;
+  if (props.error) {
+    progressBarColor = theme.color.text.error;
+  } else if (props.percent === 100) {
+    progressBarColor = theme.color.text.success;
+  }
+
+  return (
+    <Tooltip title={props.percent + "%"} style={{ verticalAlign: "middle" }} trigger={TOOLTIP_TRIGGER}>
+      <Progress
+        style={{ marginRight: "8px", verticalAlign: "middle" }}
+        type="circle"
+        size={theme.controls.heightSmall - 6}
+        percent={props.percent}
+        showInfo={false}
+        strokeColor={progressBarColor}
+        strokeWidth={12}
+      />
+    </Tooltip>
+  );
+}
