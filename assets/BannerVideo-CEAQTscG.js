dd37e8bcdad1c34793aef1aacacb0cd23a23f357
ShrimpCryptid <peytonalee@gmail.com>
2025-09-23 15:20:32 +0000

    Deploying to gh-pages from @ allen-cell-animated/timelapse-colorizer@b9864681f6df8b7be99f5f169d9c7c9f260030b7 🚀

---
+++ assets/BannerVideo-CEAQTscG.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-jhqzAsXd.js";import"./gh_routing-Ccc8RuuD.js";const o="/timelapse-colorizer/assets/banner_video-8h8QZ5tC.mp4";function s(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:o,type:"video/mp4"})})}export{s as default};
