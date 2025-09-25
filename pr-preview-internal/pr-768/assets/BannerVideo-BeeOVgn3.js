78ed994e14d1e1fdc782545cd0831a4b1cd2350a
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2025-09-25 00:52:45 +0000

    Deploy preview for PR 768 🛫

---
+++ pr-preview-internal/pr-768/assets/BannerVideo-BeeOVgn3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-BXE8tZb9.js";import"./gh_routing-Ccc8RuuD.js";const r="/timelapse-colorizer/pr-preview-internal/pr-768/assets/banner_video-8h8QZ5tC.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
