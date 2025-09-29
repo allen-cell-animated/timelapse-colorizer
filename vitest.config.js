d5a9ae810a293f88b0b280a2eff88cf2cdbb1dc1
Peyton Lee <peyton.lee@alleninstitute.org>
2025-09-29 12:13:57 -0700

    refactor: Updated sorting rules for eslint and prettier, added `@` as a supported alias for `src`

--- vitest.config.js
+++ vitest.config.js
@@ -1,20 +1,25 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import glsl from 'vite-plugin-glsl';
-import svgr from 'vite-plugin-svgr';
+import { resolve } from "node:path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [glsl(), react(), svgr()],
   deps: {
-    inline: ['vitest-canvas-mock'],
+    inline: ["vitest-canvas-mock"],
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./tests/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['text', 'json-summary', 'json'],
-    }
-  }
+      reporter: ["text", "json-summary", "json"],
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+  },
 });
