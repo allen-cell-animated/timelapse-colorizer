bc57a4fd166bf8c54a03e0669e930e390e41aac4
Peyton Lee <peyton.lee@alleninstitute.org>
2025-10-29 10:52:31 -0700

    refactor: Remove unused comment, removed deprecated `frameBorder`

--- src/routes/LandingPage/components/VideoCard.tsx
+++ src/routes/LandingPage/components/VideoCard.tsx
@@ -11,7 +11,6 @@ type VideoCardProps = {
 export default function VideoCard(props: VideoCardProps): ReactElement {
   const { entry, index } = props;
 
-  //<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><iframe src="https://www.youtube.com/embed/cip80-n5CN8?rel=0" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen scrolling="no" allow="accelerometer *; clipboard-write *; encrypted-media *; gyroscope *; picture-in-picture *; web-share *;" referrerpolicy="strict-origin"></iframe></div>
   return (
     <FlexColumn key={index} $gap={8}>
       <div>
@@ -19,9 +18,8 @@ export default function VideoCard(props: VideoCardProps): ReactElement {
           title={entry.title}
           id="ytplayer"
           width="100%"
-          style={{ aspectRatio: "16/9" }}
+          style={{ aspectRatio: "16/9", border: 0 }}
           src={entry.videoUrl}
-          frameBorder={0}
           allowFullScreen
           allow="picture-in-picture *; web-share *;"
           referrerPolicy="strict-origin"
