52b407c53c40e746efa1e53011ef15b5362d3a4b
Peyton Lee <peyton.lee@alleninstitute.org>
2025-11-03 10:52:23 -0800

    refactor: Use named constants for pixel breakpoints

--- src/routes/LandingPage/components/VideoList.tsx
+++ src/routes/LandingPage/components/VideoList.tsx
@@ -10,6 +10,10 @@ type VideoListProps = {
   videoEntries: VideoEntry[];
 };
 
+const DEFAULT_LANDING_PAGE_CONTENT_WIDTH_PX = 1060;
+const THREE_SLIDES_BREAKPOINT_PX = 1025;
+const TWO_SLIDES_BREAKPOINT_PX = 700;
+
 const StyledCarousel = styled(Carousel)`
   .slick-list {
     /* Add padding so controls don't overlap text */
@@ -33,7 +37,7 @@ export default function VideoList(props: VideoListProps): ReactElement {
 
   // Track container width to adjust number of visible slides
   const divContainerRef = useRef<HTMLDivElement>(null);
-  const [containerWidth, setContainerWidth] = useState<number>(1060);
+  const [containerWidth, setContainerWidth] = useState<number>(DEFAULT_LANDING_PAGE_CONTENT_WIDTH_PX);
   useEffect(() => {
     const onWidthChange = (): void => {
       if (divContainerRef.current) {
@@ -50,9 +54,9 @@ export default function VideoList(props: VideoListProps): ReactElement {
   // These breakpoints were chosen to keep high-res thumbnails (YouTube embeds
   // will switch to a low-res thumbnail if the width is below ~320px).
   let numSlidesToShow;
-  if (containerWidth >= 1025) {
+  if (containerWidth >= THREE_SLIDES_BREAKPOINT_PX) {
     numSlidesToShow = 3;
-  } else if (containerWidth >= 700) {
+  } else if (containerWidth >= TWO_SLIDES_BREAKPOINT_PX) {
     numSlidesToShow = 2;
   } else {
     numSlidesToShow = 1;
