cbf4c1c5fea3c17b603c553407ac536d3068c8b8
Peyton Lee <peyton.lee@alleninstitute.org>
2025-12-09 17:52:04 -0800

    refactor: Make track path length control into its own component

---
+++ src/components/Tabs/Settings/TrackPathLengthControl.tsx
@@ -0,0 +1,121 @@
+import React, { ReactElement, ReactNode } from "react";
+import styled from "styled-components";
+
+import LabeledSlider from "src/components/Inputs/LabeledSlider";
+import { MAX_SETTINGS_SLIDER_WIDTH } from "src/constants";
+import { useViewerStateStore } from "src/state";
+import { FlexColumn, FlexRowAlignCenter } from "src/styles/utils";
+
+type TrackPathLengthControlProps = {
+  id: string;
+};
+
+const PastFutureLabels = styled(FlexRowAlignCenter)`
+  position: absolute;
+  top: -10px;
+  justify-content: center;
+  width: calc(100%);
+  // Fudge number to get the labels centered between the sliders
+  padding-left: 6px;
+  gap: 5px;
+  & p {
+    font-size: var(--font-size-label-small);
+    color: var(--color-text-secondary);
+  }
+`;
+
+/**
+ * Control for adjusting the length of track paths shown in the viewer. Shown
+ * as two sliders extending left and right from a center point.
+ */
+export default function TrackPathLengthControl(props: TrackPathLengthControlProps): ReactElement {
+  const dataset = useViewerStateStore((state) => state.dataset);
+  const track = useViewerStateStore((state) => state.track);
+  const maxTrackLength = dataset?.getMaxTrackLength() ?? 1;
+  const trackStepMax = track ? track.duration() : maxTrackLength;
+
+  const trackPathPastSteps = useViewerStateStore((state) => state.trackPathPastSteps);
+  const trackPathFutureSteps = useViewerStateStore((state) => state.trackPathFutureSteps);
+  const setTrackPathPastSteps = useViewerStateStore((state) => state.setTrackPathPastSteps);
+  const setTrackPathFutureSteps = useViewerStateStore((state) => state.setTrackPathFutureSteps);
+
+  const handlePastStepsChange = (value: number) => {
+    // value == trackStepMax => set to Infinity to show full past track
+    if (value == trackStepMax) {
+      setTrackPathPastSteps(Infinity);
+    } else {
+      setTrackPathPastSteps(Math.max(0, value));
+    }
+  };
+
+  const handleFutureStepsChange = (value: number) => {
+    // value == trackStepMax => set to Infinity to show full future track
+    if (value == trackStepMax) {
+      setTrackPathFutureSteps(Infinity);
+    } else {
+      setTrackPathFutureSteps(Math.max(0, value));
+    }
+  };
+
+  const pathLengthTooltipFormatter = (value?: number): ReactNode => {
+    if (value === undefined) {
+      return null;
+    } else if (value === trackStepMax) {
+      return "All";
+    }
+    return value.toString();
+  };
+
+  return (
+    <FlexColumn style={{ alignItems: "flex-start", width: "fit-content", position: "relative" }}>
+      <PastFutureLabels>
+        <p>Past</p>
+        <p>|</p>
+        <p>Future</p>
+      </PastFutureLabels>
+      <FlexRowAlignCenter>
+        <div style={{ maxWidth: MAX_SETTINGS_SLIDER_WIDTH, width: "100%" }}>
+          <LabeledSlider
+            id={props.id}
+            type="value"
+            minSliderBound={0}
+            maxSliderBound={trackStepMax}
+            maxSliderLabel={`${trackStepMax - 1}`}
+            minInputBound={0}
+            maxInputBound={10000}
+            value={trackPathPastSteps}
+            onChange={handlePastStepsChange}
+            step={1}
+            reverse={true}
+            inputPlaceholder="All"
+            tooltipFormatter={pathLengthTooltipFormatter}
+          />
+        </div>
+        <div
+          style={{
+            maxWidth: MAX_SETTINGS_SLIDER_WIDTH,
+            width: "100%",
+            position: "relative",
+            height: "var(--button-height)",
+            marginLeft: "10px",
+          }}
+        >
+          <LabeledSlider
+            type="value"
+            minSliderBound={0}
+            maxSliderBound={trackStepMax}
+            maxSliderLabel={`${trackStepMax - 1}`}
+            minInputBound={0}
+            maxInputBound={10000}
+            value={trackPathFutureSteps}
+            onChange={handleFutureStepsChange}
+            step={1}
+            inputPosition="right"
+            inputPlaceholder="All"
+            tooltipFormatter={pathLengthTooltipFormatter}
+          />
+        </div>
+      </FlexRowAlignCenter>
+    </FlexColumn>
+  );
+}
