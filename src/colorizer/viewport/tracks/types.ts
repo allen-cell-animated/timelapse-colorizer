aaf231015f5f3c67f50f70104e3cc6a27f7240f4
Peyton Lee <peyton.lee@alleninstitute.org>
2025-12-11 14:29:32 -0800

    refactor: Create specific type for TrackPathParams

--- src/colorizer/viewport/tracks/types.ts
+++ src/colorizer/viewport/tracks/types.ts
@@ -1,21 +1,26 @@
-import type { RenderCanvasStateParams } from "src/colorizer/viewport/types";
+import type { Color } from "three";
+
+import type ColorRamp from "src/colorizer/ColorRamp";
+import type Dataset from "src/colorizer/Dataset";
+import type Track from "src/colorizer/Track";
+import type { DrawSettings, TrackPathColorMode } from "src/colorizer/types";
 
 /** Subset of RenderCanvasParams */
-export type TrackPathParams = Pick<
-  RenderCanvasStateParams,
-  | "dataset"
-  | "track"
-  | "featureKey"
-  | "colorRamp"
-  | "colorRampRange"
-  | "categoricalPaletteRamp"
-  | "inRangeLUT"
-  | "outOfRangeDrawSettings"
-  | "outlierDrawSettings"
-  | "trackPathColorMode"
-  | "outlineColor"
-  | "trackPathColor"
-  | "trackPathWidthPx"
-  | "showTrackPath"
-  | "showTrackPathBreaks"
->;
+export type TrackPathParams = {
+  dataset: Dataset | null;
+  track: Track | null;
+  featureKey: string | null;
+  colorRamp: ColorRamp;
+  colorRampRange: [number, number];
+  categoricalPaletteRamp: ColorRamp;
+  outlierDrawSettings: DrawSettings;
+  outlineColor: Color;
+  outOfRangeDrawSettings: DrawSettings;
+  inRangeLUT: Uint8Array;
+  // Track-specific settings
+  showTrackPath: boolean;
+  showTrackPathBreaks: boolean;
+  trackPathColor: Color;
+  trackPathWidthPx: number;
+  trackPathColorMode: TrackPathColorMode;
+};
