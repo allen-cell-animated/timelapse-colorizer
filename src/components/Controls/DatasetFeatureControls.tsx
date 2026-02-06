562c3d4a2fa9109d56247d801346da9673da0545
Peyton Lee <peyton.lee@alleninstitute.org>
2026-02-06 15:47:06 -0800

    feat: Added warning popup when switching datasets with annotations

--- src/components/Controls/DatasetFeatureControls.tsx
+++ src/components/Controls/DatasetFeatureControls.tsx
@@ -1,9 +1,12 @@
-import React, { type ReactElement, useMemo } from "react";
+import { Button, Popconfirm } from "antd";
+import React, { type ReactElement, useMemo, useRef, useState } from "react";
+import { useTimeout } from "usehooks-ts";
 
 import type { Dataset } from "src/colorizer";
 import SelectionDropdown from "src/components/Dropdowns/SelectionDropdown";
 import type { SelectItem } from "src/components/Dropdowns/types";
 import GlossaryPanel from "src/components/GlossaryPanel";
+import { AnnotationState } from "src/hooks";
 import { useViewerStateStore } from "src/state";
 import { FlexRow } from "src/styles/utils";
 
@@ -11,6 +14,7 @@ type DatasetFeatureControlsProps = {
   onSelectDataset: (datasetKey: string) => Promise<void>;
   onSelectFeature: (dataset: Dataset, featureKey: string) => void;
   disabled: boolean;
+  annotationState: AnnotationState;
 };
 
 export default function DatasetFeatureControls(props: DatasetFeatureControlsProps): ReactElement {
@@ -20,6 +24,11 @@ export default function DatasetFeatureControls(props: DatasetFeatureControlsProp
   const dataset = useViewerStateStore((state) => state.dataset);
   const featureKey = useViewerStateStore((state) => state.featureKey);
   const collection = useViewerStateStore((state) => state.collection);
+
+  const userConfirmationPromiseResolveRef = useRef<(() => void) | null>(null);
+  const userConfirmationPromiseRejectRef = useRef<(() => void) | null>(null);
+  const [showAnnotationDataWarning, setShowAnnotationDataWarning] = useState(false);
+  const hasAnnotations = props.annotationState.data.getLabels().length > 0;
 
   const datasetDropdownData = useMemo(() => collection?.getDatasetKeys() || [], [collection]);
   const featureDropdownData = useMemo((): SelectItem[] => {
@@ -32,6 +41,44 @@ export default function DatasetFeatureControls(props: DatasetFeatureControlsProp
     });
   }, [dataset]);
 
+  const onSelectDataset = async (key: string): Promise<void> => {
+    if (key === datasetKey) {
+      return;
+    }
+    if (hasAnnotations) {
+      setShowAnnotationDataWarning(true);
+      // Setup promise
+      const userConfirmationPromise = new Promise<void>((resolve, reject) => {
+        const resolveCallback = async (): Promise<void> => {
+          await props.onSelectDataset(key);
+          resolve();
+        };
+        userConfirmationPromiseResolveRef.current = resolveCallback;
+        userConfirmationPromiseRejectRef.current = reject;
+      });
+      return userConfirmationPromise;
+    } else {
+      await props.onSelectDataset(key);
+    }
+  };
+
+  const onConfirmDatasetChange = (): void => {
+    userConfirmationPromiseResolveRef.current?.();
+
+    setShowAnnotationDataWarning(false);
+    userConfirmationPromiseResolveRef.current = null;
+    userConfirmationPromiseRejectRef.current = null;
+  };
+
+  const onCancelDatasetChange = (): void => {
+    console.log("Cancelled");
+    userConfirmationPromiseRejectRef.current?.();
+
+    setShowAnnotationDataWarning(false);
+    userConfirmationPromiseResolveRef.current = null;
+    userConfirmationPromiseRejectRef.current = null;
+  };
+
   return (
     <FlexRow $gap={22} style={{ width: "100%" }}>
       <div style={{ width: "45%" }}>
@@ -41,9 +88,28 @@ export default function DatasetFeatureControls(props: DatasetFeatureControlsProp
           selected={datasetKey ?? ""}
           buttonType="primary"
           items={datasetDropdownData}
-          onChange={props.onSelectDataset}
+          onChange={onSelectDataset}
           controlWidth={"100%"}
         />
+        <Popconfirm
+          title={"Change dataset with existing annotations?"}
+          description={
+            <div style={{ maxWidth: 300 }}>
+              Datasets with different tracks will cause existing annotations to be applied to the wrong objects.
+              Consider exporting and clearing existing annotations first.
+              <Button>Export and clear annotations</Button>
+            </div>
+          }
+          open={showAnnotationDataWarning}
+          onOpenChange={onCancelDatasetChange}
+          onCancel={onCancelDatasetChange}
+          onConfirm={onConfirmDatasetChange}
+          okText="Continue"
+          placement="bottom"
+          style={{ width: "300px" }}
+        >
+          <div></div>
+        </Popconfirm>
       </div>
 
       <FlexRow $gap={6} style={{ width: "55%" }}>
