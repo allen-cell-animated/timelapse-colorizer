ba807285dfe6067942fc2d8102f81d2b6df36485
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2026-02-17 20:30:59 +0000

    Deploy preview for PR 876 🛫

---
+++ pr-preview/pr-876/assets/BannerVideo-B8w7sST2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-DRvahFr4.js";import"./gh_routing-Ccc8RuuD.js";const r="/timelapse-colorizer/pr-preview/pr-876/assets/banner_video-8h8QZ5tC.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
