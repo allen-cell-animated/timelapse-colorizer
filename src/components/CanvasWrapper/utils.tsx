a7a62f6b788cd207723d01c65a4b089b2ddf6250
Peyton Lee <peyton.lee@alleninstitute.org>
2026-02-17 12:42:01 -0800

    fix: Linting

--- src/components/CanvasWrapper/utils.tsx
+++ src/components/CanvasWrapper/utils.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 
 import { KeyCharacter } from "src/components/Display/ShortcutKeyText";
 import { SHORTCUT_KEYS } from "src/constants";
-import { AppTheme } from "src/styles/AppStyle";
+import type { AppTheme } from "src/styles/AppStyle";
 import { FlexRow } from "src/styles/utils";
 
-export const getBackdropChannelHotkeyHint = (theme: AppTheme) => {
+export const getBackdropChannelHotkeyHint = (theme: AppTheme): ReactNode => {
   return (
     <FlexRow style={{ color: theme.color.text.hint, width: "100%" }} $gap={4}>
       Press <KeyCharacter>{SHORTCUT_KEYS.backdropsOrChannels.cycleBackward.keycodeDisplay[0]}</KeyCharacter> /{" "}
