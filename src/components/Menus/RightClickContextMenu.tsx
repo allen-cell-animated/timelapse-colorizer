1a25f7ed01a45ceeca6d7f7d9f9b202b074eaeac
Peyton Lee <peyton.lee@alleninstitute.org>
2026-08-20 12:56:45 -0700

    feat: Added mouse following

--- src/components/Menus/RightClickContextMenu.tsx
+++ src/components/Menus/RightClickContextMenu.tsx
@@ -1,4 +1,5 @@
 import { Menu, type MenuProps, Popover } from "antd";
+import { MenuInfo } from "rc-menu/lib/interface";
 import React, { PropsWithChildren, ReactElement, useEffect, useState } from "react";
 import styled from "styled-components";
 
@@ -20,6 +21,13 @@ type ContextMenuItem = {
 };
 
 const StyledPopoverContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  pointer-events: none;
+
   & .ant-popover-inner {
     border-radius: 8px;
     overflow: hidden;
@@ -70,7 +78,7 @@ const defaultProps: Partial<RightClickContextMenuProps> = {
       key: "default",
       label: "Placeholder Item",
       onClick: () => {
-        console.log("Default item clicked");
+        console.log("Clicked placeholder item");
       },
     },
   ],
@@ -80,35 +88,66 @@ export default function RightClickContextMenu(inputProps: PropsWithChildren<Righ
   const props = { ...defaultProps, ...inputProps } as PropsWithChildren<Required<RightClickContextMenuProps>>;
   const contentContainerRef = React.useRef<HTMLDivElement>(null);
   const popoverContainerRef = React.useRef<HTMLDivElement>(null);
+  const popoverAnchorRef = React.useRef<HTMLDivElement>(null);
   const [showContextMenu, setShowContextMenu] = useState(false);
+  const showContextMenuRef = React.useRef(showContextMenu);
 
-  const onContextMenu = (ev: PointerEvent): void => {
-    ev.preventDefault();
-    ev.stopPropagation();
-    setShowContextMenu(true);
-  };
+  // MARK: Event listeners
 
   useEffect(() => {
     const container = contentContainerRef.current;
     if (!container) {
       return;
     }
+
+    const onContextMenu = (ev: PointerEvent): void => {
+      ev.preventDefault();
+      ev.stopPropagation();
+      setShowContextMenu(true);
+      showContextMenuRef.current = true;
+    };
+
+    const onMouseMove = (ev: MouseEvent): void => {
+      if (popoverAnchorRef.current && contentContainerRef.current && !showContextMenuRef.current) {
+        popoverAnchorRef.current.style.left = `${
+          ev.clientX - contentContainerRef.current.getBoundingClientRect().left
+        }px`;
+        popoverAnchorRef.current.style.top = `${
+          ev.clientY - contentContainerRef.current.getBoundingClientRect().top
+        }px`;
+      }
+    };
+
+    const onMouseDown = (ev: MouseEvent): void => {
+      // On right click, readjust position
+      if (ev.button === 2) {
+        onMouseMove(ev);
+      }
+    };
+
     container.addEventListener("contextmenu", onContextMenu);
+    container.addEventListener("mousemove", onMouseMove);
+    container.addEventListener("mousedown", onMouseDown);
     return () => {
       container.removeEventListener("contextmenu", onContextMenu);
+      container.removeEventListener("mousemove", onMouseMove);
+      container.removeEventListener("mousedown", onMouseDown);
     };
   }, []);
 
-  const onClickMenuItem = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {};
+  // MARK: Menu click handling
+
+  const keyToClickHandlerMap = getKeyToClickHandlerMap(props.items);
+  const onClickMenuItem = (info: MenuInfo): void => {
+    const clickHandler = keyToClickHandlerMap.get(info.key);
+    if (clickHandler) {
+      clickHandler(info.domEvent as React.MouseEvent<HTMLDivElement, MouseEvent>);
+    }
+  };
 
   const menu = (
     <FlexColumn>
-      <Menu
-        items={contextMenuItemsToMenuItems(props.items)}
-        onClick={(info) => {
-          console.log("Clicked ", info);
-        }}
-      ></Menu>
+      <Menu items={contextMenuItemsToMenuItems(props.items)} onClick={onClickMenuItem}></Menu>
     </FlexColumn>
   );
 
@@ -117,17 +156,21 @@ export default function RightClickContextMenu(inputProps: PropsWithChildren<Righ
       <div ref={contentContainerRef} style={{ width: "100%", height: "100%" }}>
         {props.children}
       </div>
-      <StyledPopoverContainer ref={popoverContainerRef}>
+
+      <StyledPopoverContainer ref={popoverContainerRef} id="right-click-context-menu-popover-container">
         <Popover
           content={menu}
           open={showContextMenu}
-          onOpenChange={setShowContextMenu}
+          onOpenChange={(open) => {
+            setShowContextMenu(open);
+            showContextMenuRef.current = open;
+          }}
           getPopupContainer={() => popoverContainerRef.current ?? document.body}
           placement="right"
           style={{ padding: 0 }}
           trigger={["click", "contextMenu"]}
         >
-          <div style={{ position: "absolute", top: 0, left: 0 }}></div>
+          <div ref={popoverAnchorRef} style={{ position: "absolute", height: "1px", width: "1px" }}></div>
         </Popover>
       </StyledPopoverContainer>
     </div>
