d0ba08c8acf6f9482414d2178b380dbcd7398ff0
Peyton Lee <peyton.lee@alleninstitute.org>
2026-08-25 15:58:03 -0700

    feat: Add option to expand/collapse tracks in detail view

--- src/components/Tabs/Plots/LineageGraph/tree_utils.ts
+++ src/components/Tabs/Plots/LineageGraph/tree_utils.ts
@@ -207,13 +207,16 @@ export function expandTrack(
     previouslyExpandedTracks.add(id);
     // Expand all parents of the node, up to a root node.
     forEachAncestor(id, data.trackIdToTrackInfo, relationships.idToParents, (parentData) => {
+      if (expandedTracks.has(parentData.id)) {
+        return false;
+      }
       expandedTracks.add(parentData.id);
       previouslyExpandedTracks.add(parentData.id);
       return true;
     });
     // Traverse children, expand if previously expanded too.
     forEachDescendant(id, data.trackIdToTrackInfo, relationships.idToChildren, (childData) => {
-      if (previouslyExpandedTracks.has(childData.id)) {
+      if (previouslyExpandedTracks.has(childData.id) && !expandedTracks.has(childData.id)) {
         expandedTracks.add(childData.id);
         return true;
       }
@@ -226,6 +229,19 @@ export function expandTrack(
   };
 }
 
+export function expandTracks(
+  trackIds: number[],
+  expandedState: TreeExpandedState,
+  data: LineageData,
+  relationships: LineageDataRelationships
+): TreeExpandedState {
+  let newState = expandedState;
+  for (const trackId of trackIds) {
+    newState = expandTrack(trackId, newState, data, relationships);
+  }
+  return newState;
+}
+
 /**
  * Collapses a track and all of its descendants. If the track has any coparents,
  * they will also be collapsed.
@@ -235,7 +251,9 @@ export function collapseTrack(
   trackId: number,
   expandedState: TreeExpandedState,
   data: LineageData,
-  relationships: LineageDataRelationships
+  relationships: LineageDataRelationships,
+  /** Reset expanded state for collapsed tracks, so they do not re-expand. */
+  reset?: boolean
 ): TreeExpandedState {
   const { expandedTracks: _expandedTracks, previouslyExpandedTracks: _previouslyExpandedTracks } = expandedState;
   const expandedTracks = new Set<number>(_expandedTracks);
@@ -266,6 +284,9 @@ export function collapseTrack(
         return false;
       }
       expandedTracks.delete(childData.id);
+      if (reset) {
+        previouslyExpandedTracks.delete(childData.id);
+      }
       traversedNodes.add(childData.id);
 
       // Check coparents
@@ -276,6 +297,9 @@ export function collapseTrack(
         } else {
           if (expandedTracks.has(coparentId)) {
             expandedTracks.delete(coparentId);
+            if (reset) {
+              previouslyExpandedTracks.delete(coparentId);
+            }
             traversedNodes.add(coparentId);
             collapseAllChildren(coparentId);
           }
@@ -291,6 +315,9 @@ export function collapseTrack(
             // Collapse the parent if currently expanded (and all of its
             // children)
             expandedTracks.delete(parentId);
+            if (reset) {
+              previouslyExpandedTracks.delete(parentId);
+            }
             traversedNodes.add(parentId);
             collapseAllChildren(parentId);
           }
