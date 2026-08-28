ca73b1654f4152414692e013fa0857cdd30569d5
Peyton Lee <peyton.lee@alleninstitute.org>
2026-08-28 15:11:33 -0700

    refactor: Renamed "options" to "selection options"

--- src/components/Tabs/Plots/LineageGraph/lineage_utils.ts
+++ src/components/Tabs/Plots/LineageGraph/lineage_utils.ts
@@ -304,7 +304,7 @@ export function getLineageContextMenuItems(
         visible: areTrackAndAllChildrenSelected,
       },
       {
-        label: "Options...",
+        label: "Selection options...",
         children: [
           {
             label: (!data.applyTrackColorToRelatives ? "☑ " : "☐ ") + "Use random outline colors",
