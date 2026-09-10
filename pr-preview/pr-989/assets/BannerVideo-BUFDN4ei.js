c823faff579e4b47d15ac98bbbca7aaa0b0e34dc
ShrimpCryptid <ShrimpCryptid@users.noreply.github.com>
2026-09-10 20:13:12 +0000

    Deploy preview for PR 989 🛫

---
+++ pr-preview/pr-989/assets/BannerVideo-BUFDN4ei.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-1h7IBdix.js";import"./gh_routing-DSAAfHln.js";const r="/timelapse-colorizer/pr-preview/pr-989/assets/banner_video-8h8QZ5tC.mp4";function i(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{i as default};
